e734407ba3b061521181d3c3e3ab85f5d7fbd592
Dido Sevilla <dido@stormwyrm.com>
2018-11-11 22:05:20 +0800

    test string apply, for both ordinary characters and extended Unicode

---
+++ src/test/java/com/stormwyrm/nekoarc/types/AStringTest.java
@@ -0,0 +1,55 @@
+package com.stormwyrm.nekoarc.types;
+
+import com.stormwyrm.nekoarc.vm.VirtualMachine;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AStringTest {
+    @Test
+    public void testApply() {
+        AString str = new AString("日本語");
+        // Apply the above string
+        byte inst[] = {(byte)0xca, 0x00, 0x00, 0x00,	// env 0 0 0
+                0x44, 0x01, 0x00, 0x00, 0x00,			// ldi 1
+                0x01,									// push
+                0x43, 0x00, 0x00, 0x00, 0x00,			// ldl 0
+                0x4c, 0x01,								// apply 1
+                0x0d									// ret
+        };
+        VirtualMachine vm = new VirtualMachine(1024);
+        ArcObject literals[] = new ArcObject[1];
+        literals[0] = str;
+        vm.load(inst, 0, literals);
+        vm.setargc(0);
+        assertTrue(vm.runnable());
+        vm.run();
+        assertFalse(vm.runnable());
+        assertEquals(0x672c, ((Rune)vm.getAcc()).rune);
+        assertEquals("#\\本", vm.getAcc().toString());
+    }
+
+    @Test
+    public void testUnicodeApply() {
+        AString str = new AString("\uD83D\uDE1D\uD83D\uDE0E");
+        // Apply the above string
+        byte inst[] = {(byte)0xca, 0x00, 0x00, 0x00,	// env 0 0 0
+                0x44, 0x00, 0x00, 0x00, 0x00,			// ldi 0
+                0x01,									// push
+                0x43, 0x00, 0x00, 0x00, 0x00,			// ldl 0
+                0x4c, 0x01,								// apply 1
+                0x0d									// ret
+        };
+        VirtualMachine vm = new VirtualMachine(1024);
+        ArcObject literals[] = new ArcObject[1];
+        literals[0] = str;
+        vm.load(inst, 0, literals);
+        vm.setargc(0);
+        assertTrue(vm.runnable());
+        vm.run();
+        assertFalse(vm.runnable());
+        assertEquals(0x1f61d, ((Rune)vm.getAcc()).rune);
+        assertEquals("#\\\uD83D\uDE1D", vm.getAcc().toString());
+
+    }
+}
