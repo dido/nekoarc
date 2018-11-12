f1a60192e8aa951e270fb021d73e7d51349afda2
Dido Sevilla <dido@stormwyrm.com>
2018-11-12 15:21:12 +0800

    scdr test

---
+++ src/test/java/com/stormwyrm/nekoarc/functions/ScdrTest.java
@@ -0,0 +1,40 @@
+package com.stormwyrm.nekoarc.functions;
+
+import com.stormwyrm.nekoarc.Nil;
+import com.stormwyrm.nekoarc.types.ArcObject;
+import com.stormwyrm.nekoarc.types.Cons;
+import com.stormwyrm.nekoarc.types.Fixnum;
+import com.stormwyrm.nekoarc.vm.VirtualMachine;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ScdrTest {
+    @Test
+    public void test() {
+        Cons cons = new Cons(Fixnum.get(1), new Cons(Fixnum.get(2), new Cons(Fixnum.get(3), new Cons(Fixnum.get(4), Nil.NIL))));
+        Scdr scdr = Scdr.SCDR;
+        // Essentially (scdr '(1 2 3 4) nil)
+        byte inst[] = {(byte) 0xca, 0x00, 0x00, 0x00,    // env 0 0 0
+                0x43, 0x00, 0x00, 0x00, 0x00,            // ldl 0
+                0x01,                                    // push
+                0x13,                                   // nil
+                0x01,                                   // push
+                0x43, 0x01, 0x00, 0x00, 0x00,            // ldl 1
+                0x4c, 0x02,                                // apply 1
+                0x0d                                    // ret
+        };
+        VirtualMachine vm = new VirtualMachine(1024);
+        ArcObject literals[] = new ArcObject[2];
+        literals[0] = cons;
+        literals[1] = scdr;
+        vm.load(inst, 0, literals);
+        vm.setargc(0);
+        assertTrue(vm.runnable());
+        vm.run();
+        assertFalse(vm.runnable());
+        assertEquals(Nil.NIL, vm.getAcc());
+        assertEquals(1, ((Fixnum) cons.car()).fixnum);
+        assertEquals(Nil.NIL, cons.cdr());
+    }
+}
