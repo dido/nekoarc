78de8376f64ccafb5a1814f8133026ef0bd78c5e
Rafael Sevilla <rsevilla@virtela.net>
2018-11-13 11:10:44 +0800

    FTable builtin

---
+++ src/main/java/com/stormwyrm/nekoarc/functions/FTable.java
@@ -0,0 +1,22 @@
+package com.stormwyrm.nekoarc.functions;
+
+import com.stormwyrm.nekoarc.InvokeThread;
+import com.stormwyrm.nekoarc.types.ArcObject;
+import com.stormwyrm.nekoarc.types.Table;
+
+public class FTable extends Builtin {
+    public static final FTable INSTANCE = new FTable();
+
+    private FTable() {
+        super("table", 0, 0, 0, false);
+    }
+
+    public static Builtin getInstance() {
+        return(INSTANCE);
+    }
+
+    @Override
+    public ArcObject invoke(InvokeThread vm) {
+        return(new Table());
+    }
+}
