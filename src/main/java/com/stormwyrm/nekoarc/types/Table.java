4af9eb9d0da7685cf854903643b6309ccfd3a01a
Rafael Sevilla <rsevilla@virtela.net>
2018-11-13 10:17:45 +0800

    tables

---
+++ src/main/java/com/stormwyrm/nekoarc/types/Table.java
@@ -0,0 +1,42 @@
+package com.stormwyrm.nekoarc.types;
+
+import com.stormwyrm.nekoarc.InvokeThread;
+import com.stormwyrm.nekoarc.util.ObjectMap;
+
+public class Table extends ArcObject {
+    public static final ArcObject TYPE = Symbol.intern("vector");
+    private ObjectMap<ArcObject, ArcObject> table;
+
+    public Table() {
+        table = new ObjectMap<>();
+    }
+
+    public ArcObject get(ArcObject key) {
+        return(table.get(key));
+    }
+
+    public ArcObject put(ArcObject value, ArcObject key) {
+        return(table.put(key, value));
+    }
+
+    @Override
+    public ArcObject type() {
+        return(TYPE);
+    }
+
+    @Override
+    public long len() {
+        return(table.size);
+    }
+
+    @Override
+    public ArcObject sref(ArcObject value, ArcObject index) {
+        return(this.put(index, value));
+    }
+
+    @Override
+    public ArcObject invoke(InvokeThread thr) {
+        return(this.get(thr.getenv(0, 0)));
+    }
+
+}
