1589b43711ce9c8e48333fe8f1274e9ee55dc355
Rafael Sevilla <rsevilla@virtela.net>
2018-11-15 16:05:07 +0800

    Greater-Than predicate

---
+++ src/main/java/com/stormwyrm/nekoarc/functions/GreaterThan.java
@@ -0,0 +1,38 @@
+package com.stormwyrm.nekoarc.functions;
+
+import com.stormwyrm.nekoarc.InvokeThread;
+import com.stormwyrm.nekoarc.NekoArcException;
+import com.stormwyrm.nekoarc.Nil;
+import com.stormwyrm.nekoarc.True;
+import com.stormwyrm.nekoarc.types.ArcObject;
+import com.stormwyrm.nekoarc.types.Orderable;
+
+public class GreaterThan extends Builtin {
+    private static final GreaterThan INSTANCE = new GreaterThan();
+
+    public static Builtin getInstance() {
+        return(INSTANCE);
+    }
+
+    private GreaterThan() {
+        super(">", 0, 0, 0, true);
+    }
+
+    @Override
+    public ArcObject invoke(InvokeThread vm) {
+        if (vm.argc() < 2)
+            return(True.T);
+        ArcObject args = vm.getenv(0, 0);
+        ArcObject x = args.car();
+        args = args.cdr();
+        if (!(x instanceof Orderable))
+            throw new NekoArcException("invalid comparison, " + x + " does not have order");
+        Orderable o = (Orderable)x;
+        while (!args.is(Nil.NIL)) {
+            if (o.lessThan(args.car()) || x.is(args))
+                return(Nil.NIL);
+            args = args.cdr();
+        }
+        return(True.T);
+    }
+}
