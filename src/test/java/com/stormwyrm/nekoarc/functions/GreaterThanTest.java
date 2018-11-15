0ed23edfb36dc9727e9aed94d81a81e61c16a46f
Rafael Sevilla <rsevilla@virtela.net>
2018-11-15 16:04:44 +0800

    Tests for >

---
+++ src/test/java/com/stormwyrm/nekoarc/functions/GreaterThanTest.java
@@ -0,0 +1,296 @@
+package com.stormwyrm.nekoarc.functions;
+
+import com.stormwyrm.nekoarc.Nil;
+import com.stormwyrm.nekoarc.True;
+import com.stormwyrm.nekoarc.types.ArcObject;
+import com.stormwyrm.nekoarc.types.Flonum;
+import com.stormwyrm.nekoarc.types.Symbol;
+import com.stormwyrm.nekoarc.vm.VirtualMachine;
+import com.stormwyrm.nekoarc.vm.instruction.TRUE;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GreaterThanTest {
+    @Test
+    public void test0() {
+        byte inst[] = {(byte)0xca, 0x00, 0x00, 0x00,	// env 0 0 0
+                0x45, 0x00, 0x00, 0x00, 0x00,			// ldg 0
+                0x4c, 0x00,								// apply 0
+                0x0d									// ret
+        };
+        VirtualMachine vm = new VirtualMachine(1024);
+        vm.initSyms();
+        ArcObject literals[] = new ArcObject[1];
+        literals[0] = Symbol.intern(">");
+        vm.load(inst, 0, literals);
+        vm.setargc(0);
+        assertTrue(vm.runnable());
+        vm.run();
+        assertFalse(vm.runnable());
+        assertEquals(True.T, vm.getAcc());
+    }
+
+    @Test
+    public void test1Fixnum() {
+        byte inst[] = {(byte)0xca, 0x00, 0x00, 0x00,	// env 0 0 0
+                0x44, 0x01, 0x00, 0x00, 0x00,			// ldi 1
+                0x01,									// push
+                0x45, 0x00, 0x00, 0x00, 0x00,			// ldg 0
+                0x4c, 0x01,								// apply 1
+                0x0d									// ret
+        };
+        VirtualMachine vm = new VirtualMachine(1024);
+        vm.initSyms();
+        ArcObject literals[] = new ArcObject[1];
+        literals[0] = Symbol.intern(">");
+        vm.load(inst, 0, literals);
+        vm.setargc(0);
+        assertTrue(vm.runnable());
+        vm.run();
+        assertFalse(vm.runnable());
+        assertEquals(True.T, vm.getAcc());
+    }
+
+    @Test
+    public void test1Flonum() {
+        byte inst[] = {(byte)0xca, 0x00, 0x00, 0x00,	// env 0 0 0
+                0x43, 0x01, 0x00, 0x00, 0x00,			// ldl 1
+                0x01,									// push
+                0x45, 0x00, 0x00, 0x00, 0x00,			// ldg 0
+                0x4c, 0x01,								// apply 1
+                0x0d									// ret
+        };
+        VirtualMachine vm = new VirtualMachine(1024);
+        vm.initSyms();
+        ArcObject literals[] = new ArcObject[2];
+        literals[0] = Symbol.intern(">");
+        literals[1] = new Flonum(3.14159);
+        vm.load(inst, 0, literals);
+        vm.setargc(0);
+        assertTrue(vm.runnable());
+        vm.run();
+        assertFalse(vm.runnable());
+        assertEquals(True.T, vm.getAcc());
+    }
+
+    @Test
+    public void test2aFixnum() {
+        byte inst[] = {(byte)0xca, 0x00, 0x00, 0x00,	// env 0 0 0
+                0x44, 0x01, 0x00, 0x00, 0x00,			// ldi 1
+                0x01,									// push
+                0x44, 0x02, 0x00, 0x00, 0x00,			// ldi 2
+                0x01,									// push
+                0x45, 0x00, 0x00, 0x00, 0x00,			// ldg 0
+                0x4c, 0x02,								// apply 2
+                0x0d									// ret
+        };
+        VirtualMachine vm = new VirtualMachine(1024);
+        vm.initSyms();
+        ArcObject literals[] = new ArcObject[1];
+        literals[0] = Symbol.intern(">");
+        vm.load(inst, 0, literals);
+        vm.setargc(0);
+        assertTrue(vm.runnable());
+        vm.run();
+        assertFalse(vm.runnable());
+        assertEquals(Nil.NIL, vm.getAcc());
+    }
+
+    @Test
+    public void test2bFixnum() {
+        byte inst[] = {(byte)0xca, 0x00, 0x00, 0x00,	// env 0 0 0
+                0x44, 0x01, 0x00, 0x00, 0x00,			// ldi 1
+                0x01,									// push
+                0x44, 0x00, 0x00, 0x00, 0x00,			// ldi 0
+                0x01,									// push
+                0x45, 0x00, 0x00, 0x00, 0x00,			// ldg 0
+                0x4c, 0x02,								// apply 2
+                0x0d									// ret
+        };
+        VirtualMachine vm = new VirtualMachine(1024);
+        vm.initSyms();
+        ArcObject literals[] = new ArcObject[1];
+        literals[0] = Symbol.intern(">");
+        vm.load(inst, 0, literals);
+        vm.setargc(0);
+        assertTrue(vm.runnable());
+        vm.run();
+        assertFalse(vm.runnable());
+        assertEquals(True.T, vm.getAcc());
+    }
+
+    @Test
+    public void test2aFixnumFlonum() {
+        byte inst[] = {(byte)0xca, 0x00, 0x00, 0x00,	// env 0 0 0
+                0x44, 0x01, 0x00, 0x00, 0x00,			// ldi 1
+                0x01,									// push
+                0x43, 0x01, 0x00, 0x00, 0x00,			// ldl 1
+                0x01,									// push
+                0x45, 0x00, 0x00, 0x00, 0x00,			// ldg 0
+                0x4c, 0x02,								// apply 2
+                0x0d									// ret
+        };
+        VirtualMachine vm = new VirtualMachine(1024);
+        vm.initSyms();
+        ArcObject literals[] = new ArcObject[2];
+        literals[0] = Symbol.intern(">");
+        literals[1] = new Flonum(1e100);
+        vm.load(inst, 0, literals);
+        vm.setargc(0);
+        assertTrue(vm.runnable());
+        vm.run();
+        assertFalse(vm.runnable());
+        assertEquals(Nil.NIL, vm.getAcc());
+    }
+
+    @Test
+    public void test2bFixnumFlonum() {
+        byte inst[] = {(byte)0xca, 0x00, 0x00, 0x00,	// env 0 0 0
+                0x44, 0x01, 0x00, 0x00, 0x00,			// ldi 1
+                0x01,									// push
+                0x43, 0x01, 0x00, 0x00, 0x00,			// ldl 1
+                0x01,									// push
+                0x45, 0x00, 0x00, 0x00, 0x00,			// ldg 0
+                0x4c, 0x02,								// apply 2
+                0x0d									// ret
+        };
+        VirtualMachine vm = new VirtualMachine(1024);
+        vm.initSyms();
+        ArcObject literals[] = new ArcObject[2];
+        literals[0] = Symbol.intern(">");
+        literals[1] = new Flonum(0.31831);
+        vm.load(inst, 0, literals);
+        vm.setargc(0);
+        assertTrue(vm.runnable());
+        vm.run();
+        assertFalse(vm.runnable());
+        assertEquals(True.T, vm.getAcc());
+    }
+
+    @Test
+    public void test2aFlonum() {
+        byte inst[] = {(byte)0xca, 0x00, 0x00, 0x00,	// env 0 0 0
+                0x43, 0x01, 0x00, 0x00, 0x00,			// ldl 1
+                0x01,									// push
+                0x43, 0x02, 0x00, 0x00, 0x00,			// ldl 2
+                0x01,									// push
+                0x45, 0x00, 0x00, 0x00, 0x00,			// ldg 0
+                0x4c, 0x02,								// apply 2
+                0x0d									// ret
+        };
+        VirtualMachine vm = new VirtualMachine(1024);
+        vm.initSyms();
+        ArcObject literals[] = new ArcObject[3];
+        literals[0] = Symbol.intern(">");
+        literals[1] = new Flonum(1e100);
+        literals[2] = new Flonum(1e101);
+        vm.load(inst, 0, literals);
+        vm.setargc(0);
+        assertTrue(vm.runnable());
+        vm.run();
+        assertFalse(vm.runnable());
+        assertEquals(Nil.NIL, vm.getAcc());
+    }
+
+    @Test
+    public void test2bFlonum() {
+        byte inst[] = {(byte)0xca, 0x00, 0x00, 0x00,	// env 0 0 0
+                0x43, 0x01, 0x00, 0x00, 0x00,			// ldi 1
+                0x01,									// push
+                0x43, 0x02, 0x00, 0x00, 0x00,			// ldl 2
+                0x01,									// push
+                0x45, 0x00, 0x00, 0x00, 0x00,			// ldg 0
+                0x4c, 0x02,								// apply 2
+                0x0d									// ret
+        };
+        VirtualMachine vm = new VirtualMachine(1024);
+        vm.initSyms();
+        ArcObject literals[] = new ArcObject[3];
+        literals[0] = Symbol.intern(">");
+        literals[1] = new Flonum(1e100);
+        literals[2] = new Flonum(1e-100);
+        vm.load(inst, 0, literals);
+        vm.setargc(0);
+        assertTrue(vm.runnable());
+        vm.run();
+        assertFalse(vm.runnable());
+        assertEquals(True.T, vm.getAcc());
+    }
+
+    @Test
+    public void test3aFixnum() {
+        byte inst[] = {(byte)0xca, 0x00, 0x00, 0x00,	// env 0 0 0
+                0x44, 0x01, 0x00, 0x00, 0x00,			// ldi 1
+                0x01,									// push
+                0x44, 0x02, 0x00, 0x00, 0x00,			// ldi 2
+                0x01,									// push
+                0x44, 0x03, 0x00, 0x00, 0x00,			// ldi 3
+                0x01,									// push
+                0x45, 0x00, 0x00, 0x00, 0x00,			// ldg 0
+                0x4c, 0x02,								// apply 3
+                0x0d									// ret
+        };
+        VirtualMachine vm = new VirtualMachine(1024);
+        vm.initSyms();
+        ArcObject literals[] = new ArcObject[1];
+        literals[0] = Symbol.intern(">");
+        vm.load(inst, 0, literals);
+        vm.setargc(0);
+        assertTrue(vm.runnable());
+        vm.run();
+        assertFalse(vm.runnable());
+        assertEquals(Nil.NIL, vm.getAcc());
+    }
+
+    @Test
+    public void test3bFixnum() {
+        byte inst[] = {(byte)0xca, 0x00, 0x00, 0x00,	// env 0 0 0
+                0x44, 0x01, 0x00, 0x00, 0x00,			// ldi 1
+                0x01,									// push
+                0x44, 0x02, 0x00, 0x00, 0x00,			// ldi 2
+                0x01,									// push
+                0x44, 0x00, 0x00, 0x00, 0x00,			// ldi 0
+                0x01,									// push
+                0x45, 0x00, 0x00, 0x00, 0x00,			// ldg 0
+                0x4c, 0x02,								// apply 3
+                0x0d									// ret
+        };
+        VirtualMachine vm = new VirtualMachine(1024);
+        vm.initSyms();
+        ArcObject literals[] = new ArcObject[1];
+        literals[0] = Symbol.intern(">");
+        vm.load(inst, 0, literals);
+        vm.setargc(0);
+        assertTrue(vm.runnable());
+        vm.run();
+        assertFalse(vm.runnable());
+        assertEquals(True.T, vm.getAcc());
+    }
+
+    @Test
+    public void test3cFixnum() {
+        byte inst[] = {(byte)0xca, 0x00, 0x00, 0x00,	// env 0 0 0
+                0x44, 0x01, 0x00, 0x00, 0x00,			// ldi 1
+                0x01,									// push
+                0x44, 0x03, 0x00, 0x00, 0x00,			// ldi 3
+                0x01,									// push
+                0x44, 0x00, 0x00, 0x00, 0x00,			// ldi 0
+                0x01,									// push
+                0x45, 0x00, 0x00, 0x00, 0x00,			// ldg 0
+                0x4c, 0x02,								// apply 3
+                0x0d									// ret
+        };
+        VirtualMachine vm = new VirtualMachine(1024);
+        vm.initSyms();
+        ArcObject literals[] = new ArcObject[1];
+        literals[0] = Symbol.intern(">");
+        vm.load(inst, 0, literals);
+        vm.setargc(0);
+        assertTrue(vm.runnable());
+        vm.run();
+        assertFalse(vm.runnable());
+        assertEquals(True.T, vm.getAcc());
+    }
+
+}
