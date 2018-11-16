cc2777bcf845b6e24f5a47f4db09d6a16c173bb9
Rafael Sevilla <rsevilla@virtela.net>
2018-11-16 11:28:33 +0800

    Input port abstract class

---
+++ src/main/java/com/stormwyrm/nekoarc/types/InputPort.java
@@ -0,0 +1,14 @@
+package com.stormwyrm.nekoarc.types;
+
+public abstract class InputPort extends ArcObject {
+    public static final ArcObject TYPE = Symbol.intern("input");
+
+    @Override
+    public ArcObject type() {
+        return(TYPE);
+    }
+
+    abstract int readb();
+    abstract Rune readc();
+    abstract Rune peekc();
+}
