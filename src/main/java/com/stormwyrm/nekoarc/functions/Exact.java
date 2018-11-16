01a8838a3f36052be4af04ea22c292a20416d312
Rafael Sevilla <rsevilla@virtela.net>
2018-11-16 10:35:31 +0800

    'exact' builtin

---
+++ src/main/java/com/stormwyrm/nekoarc/functions/Exact.java
@@ -0,0 +1,21 @@
+package com.stormwyrm.nekoarc.functions;
+
+import com.stormwyrm.nekoarc.InvokeThread;
+import com.stormwyrm.nekoarc.types.ArcObject;
+
+public class Exact extends Builtin {
+    private static final Exact INSTANCE = new Exact();
+
+    private Exact() {
+        super("exact", 1, 0, 0, false);
+    }
+
+    public static Builtin getInstance() {
+        return(INSTANCE);
+    }
+
+    @Override
+    public ArcObject invoke(InvokeThread vm) {
+        return(vm.getenv(0,0).rep());
+    }
+}
