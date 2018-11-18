c3eecb44c63fb19879f44e7576f1a20e637aea64
Dido Sevilla <dido@stormwyrm.com>
2018-11-18 14:45:28 +0800

    iterator test

--- src/test/java/com/stormwyrm/nekoarc/types/VectorTest.java
+++ src/test/java/com/stormwyrm/nekoarc/types/VectorTest.java
@@ -1,6 +1,9 @@
 package com.stormwyrm.nekoarc.types;
 
+import com.stormwyrm.nekoarc.Nil;
 import org.junit.Test;
+
+import java.util.Iterator;
 
 import static org.junit.Assert.*;
 
@@ -15,4 +18,16 @@ public class VectorTest {
         assertEquals("[1 2 3 4 5]", v.toString());
     }
 
+    @Test
+    public void testIterator() {
+        int len = 5;
+        Vector v = new Vector(len);
+
+        for (int i=0; i<len; i++)
+            v.setIndex(i, Fixnum.get(i));
+        Iterator<ArcObject> iter = v.iterator();
+        for (int i=0; i<len; i++)
+            assertEquals(i, Fixnum.cast(iter.next(), Nil.NIL).fixnum);
+        assertFalse(iter.hasNext());
+    }
 }
