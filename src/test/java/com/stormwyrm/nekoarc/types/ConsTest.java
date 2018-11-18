3ad23276e481f9a2569d98055edf3a8a6a73fbeb
Dido Sevilla <dido@stormwyrm.com>
2018-11-18 14:20:43 +0800

    reflexive identity tests

--- src/test/java/com/stormwyrm/nekoarc/types/ConsTest.java
+++ src/test/java/com/stormwyrm/nekoarc/types/ConsTest.java
@@ -42,4 +42,27 @@ public class ConsTest {
         c = new Cons(new Cons(Fixnum.get(1), Fixnum.get(2)), new Cons(Fixnum.get(3), Fixnum.get(4)));
         assertEquals("((1 . 2) 3 . 4)", c.toString());
     }
+
+    @Test
+    public void testIso() {
+        Cons c1, c2;
+
+        c1 = new Cons(Fixnum.get(1), new Cons(Fixnum.get(2), Nil.NIL));
+        c2 = new Cons(Fixnum.get(1), new Cons(Fixnum.get(2), Nil.NIL));
+        assertTrue(c1.is(c1));
+        assertTrue(c1.iso(c1));
+        assertFalse(c1.is(c2));
+        assertTrue(c1.iso(c2));
+        assertTrue(c2.iso(c1));
+        c2 = new Cons(Fixnum.get(1), new Cons(Fixnum.get(3), Nil.NIL));
+        assertFalse(c1.is(c2));
+        assertFalse(c1.iso(c2));
+        assertFalse(c2.iso(c1));
+
+        c2 = new Cons(Fixnum.get(1), Nil.NIL);
+        assertFalse(c1.is(c2));
+        assertFalse(c1.iso(c2));
+        assertFalse(c2.iso(c1));
+
+    }
 }
