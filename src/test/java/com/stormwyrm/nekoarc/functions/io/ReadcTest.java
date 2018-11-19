e1103c24a5137db6a088bd528b66934cab76e1d7
Rafael Sevilla <rsevilla@virtela.net>
2018-11-19 11:55:38 +0800

    Basic test for readc

---
+++ src/test/java/com/stormwyrm/nekoarc/functions/io/ReadcTest.java
@@ -0,0 +1,31 @@
+package com.stormwyrm.nekoarc.functions.io;
+
+import com.stormwyrm.nekoarc.types.*;
+import com.stormwyrm.nekoarc.vm.VirtualMachine;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReadcTest {
+    @Test
+    public void test() {
+        byte inst[] = {(byte) 0xca, 0x00, 0x00, 0x00,   // env 0 0 0
+                0x43, 0x01, 0x00, 0x00, 0x00,            // ldl 1
+                0x01,                                   // push
+                0x45, 0x00, 0x00, 0x00, 0x00,           // ldg 0
+                0x4c, 0x01,                             // apply 1
+                0x0d                                    // ret
+        };
+        VirtualMachine vm = new VirtualMachine(1024);
+        vm.initSyms();
+        ArcObject literals[] = new ArcObject[2];
+        literals[0] = Symbol.intern("readc");
+        literals[1] = new InString("蛟龍");
+        vm.load(inst, 0, literals);
+        vm.setargc(0);
+        assertTrue(vm.runnable());
+        vm.run();
+        assertTrue(vm.getAcc() instanceof Rune);
+        assertEquals(0x86df, ((Rune)vm.getAcc()).rune);
+    }
+}
