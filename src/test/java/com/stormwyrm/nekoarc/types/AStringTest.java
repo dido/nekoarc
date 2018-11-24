53385a05a4c0724fd1d202a4343e7865ffa800bb
Dido Sevilla <dido@stormwyrm.com>
2018-11-24 15:10:09 +0800

    Compare AStrings properly

--- src/test/java/com/stormwyrm/nekoarc/types/AStringTest.java
+++ src/test/java/com/stormwyrm/nekoarc/types/AStringTest.java
@@ -1,3 +1,20 @@
+/*  Copyright (C) 2018 Rafael R. Sevilla
+
+    This file is part of NekoArc
+
+    NekoArc is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; either version 3 of the
+    License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
 package com.stormwyrm.nekoarc.types;
 
 import com.stormwyrm.nekoarc.vm.VirtualMachine;
@@ -51,5 +68,13 @@ public class AStringTest {
         assertEquals(0x1f61d, ((Rune)vm.getAcc()).rune);
         assertEquals("#\\\uD83D\uDE1D", vm.getAcc().toString());
 
+    }
+
+    @Test
+    public void testSref() {
+        AString str = new AString("蛟竜");
+        ArcObject obj = str.sref(Rune.get(0x9f8d), Fixnum.get(1));
+        assertEquals("蛟龍", str.toString());
+        assertTrue(obj.is(Rune.get(0x9f8d)));
     }
 }
