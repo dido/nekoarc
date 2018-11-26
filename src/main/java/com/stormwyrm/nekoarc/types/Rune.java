ab64951ebf2974792b9ce7d3b5f17dbcb111d161
Rafael Sevilla <rsevilla@virtela.net>
2018-11-26 15:28:08 +0800

    remove weakeraccess

--- src/main/java/com/stormwyrm/nekoarc/types/Rune.java
+++ src/main/java/com/stormwyrm/nekoarc/types/Rune.java
@@ -1,3 +1,20 @@
+/*  Copyright (C) 2018 Rafael R. Sevilla
+
+    This file is part of NekoArc
+
+    NekoArc is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; either version 3 of the
+    License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
 package com.stormwyrm.nekoarc.types;
 
 import com.stormwyrm.nekoarc.NekoArcException;
@@ -8,15 +25,13 @@ import java.lang.ref.WeakReference;
 
 public class Rune extends ArcObject {
     public final ArcObject TYPE = Symbol.intern("rune");
-    @SuppressWarnings("WeakerAccess")
     public final int rune;
     private static final LongMap<WeakReference<Rune>> table = new LongMap<>();
     private static final ReferenceQueue<Rune> rq = new ReferenceQueue<>();
 
     private Rune(int x) { rune = x; }
 
-    public static Rune get(int x)
-    {
+    public static Rune get(int x) {
         Rune r;
 
         if (x < 0)
