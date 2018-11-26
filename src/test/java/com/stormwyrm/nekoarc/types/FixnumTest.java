30da0f8fed99b83ca8a0fcf94cadda3c827a9c12
Rafael Sevilla <rsevilla@virtela.net>
2018-11-26 14:08:43 +0800

    coerce test

---
+++ src/test/java/com/stormwyrm/nekoarc/types/FixnumTest.java
@@ -0,0 +1,47 @@
+/*  Copyright (C) 2018 Rafael R. Sevilla
+
+    This file is part of NekoArc
+
+    NekoArc is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; either version 3 of the
+    License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+package com.stormwyrm.nekoarc.types;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FixnumTest {
+    @Test
+    public void testCoerce() {
+        Fixnum f = Fixnum.get(123);
+        ArcObject result;
+
+        result = f.coerce(Symbol.intern("fixnum"));
+        assertEquals("fixnum", result.type().toString());
+        assertEquals(result, f);
+
+        result = f.coerce(Symbol.intern("flonum"));
+        assertEquals("flonum", result.type().toString());
+        assertEquals(((Flonum)result).flonum, 123.0, 1e-6);
+
+        result = f.coerce(Symbol.intern("string"));
+        assertEquals("string", result.type().toString());
+        assertEquals("123", result.toString());
+
+        result = f.coerce(Symbol.intern("rune"));
+        assertEquals("rune", result.type().toString());
+        assertEquals("#\\{", result.toString());
+    }
+
+}
