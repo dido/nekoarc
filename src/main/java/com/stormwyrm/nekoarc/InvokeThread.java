e8550eabc97899500afe223b0027ce865660ceb8
Rafael Sevilla <rsevilla@virtela.net>
2018-11-26 14:10:02 +0800

    add a single-arg getenv (most common use case)

--- src/main/java/com/stormwyrm/nekoarc/InvokeThread.java
+++ src/main/java/com/stormwyrm/nekoarc/InvokeThread.java
@@ -1,11 +1,29 @@
+
+/*  Copyright (C) 2018 Rafael R. Sevilla
+
+    This file is part of NekoArc
+
+    NekoArc is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; either version 3 of the
+    License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
 package com.stormwyrm.nekoarc;
 
 import com.stormwyrm.nekoarc.types.ArcObject;
 import com.stormwyrm.nekoarc.util.Callable;
 import com.stormwyrm.nekoarc.vm.VirtualMachine;
 
-/** The main reason this class exists is that Java is a weak-sauce language that doesn't have closures or true continuations. We have to
- *  emulate them using threads. */
+/** The main reason this class exists is that Java is a weak-sauce language that doesn't have closures or true
+ *  continuations. We have to emulate them using threads. */
 public class InvokeThread extends Thread
 {
 	private final Callable caller;
@@ -36,8 +54,13 @@ public class InvokeThread extends Thread
 		return(vm.getenv(i, j));
 	}
 
-	/** Perform a call to some Arc object. This should prolly work for ANY ArcObject that has a proper invoke method defined.  If it is a built-in or
-	 *  some function defined in Java, that function will run in its own thread while the current object's thread is suspended. */
+    public ArcObject getenv(int j) {
+        return(getenv(0, j));
+    }
+
+	/** Perform a call to some Arc object. This should prolly work for ANY ArcObject that has a proper invoke method
+     *  defined.  If it is a built-in or some function defined in Java, that function will run in its own thread while
+     *  the current object's thread is suspended. */
 	public ArcObject apply(ArcObject fn, ArcObject...args)
 	{
 		// First, push all of the arguments to the stack
