0e7f5f2066c6d4a5605d2f841110a7a6cb0aa79b
Rafael Sevilla <rsevilla@virtela.net>
2018-11-28 13:30:24 +0800

    rename to ReadB

--- src/main/java/com/stormwyrm/nekoarc/functions/io/ReadB.java
+++ src/main/java/com/stormwyrm/nekoarc/functions/io/ReadB.java
@@ -4,14 +4,14 @@ import com.stormwyrm.nekoarc.InvokeThread;
 import com.stormwyrm.nekoarc.functions.Builtin;
 import com.stormwyrm.nekoarc.types.*;
 
-public class Readb extends Builtin {
-    private static final Readb INSTANCE = new Readb();
+public class ReadB extends Builtin {
+    private static final ReadB INSTANCE = new ReadB();
 
     public static Builtin getInstance() {
         return(INSTANCE);
     }
 
-    private Readb() {
+    private ReadB() {
         super("readb", 0, 0, 0, true);
     }
 
