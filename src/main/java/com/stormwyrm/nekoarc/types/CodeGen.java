be5e7ebdd0ca79ea8561eb6fc0add9048059d633
Rafael Sevilla <rsevilla@virtela.net>
2018-12-04 13:40:54 +0800

    javadoc

--- src/main/java/com/stormwyrm/nekoarc/types/CodeGen.java
+++ src/main/java/com/stormwyrm/nekoarc/types/CodeGen.java
@@ -15,8 +15,6 @@
     You should have received a copy of the GNU Lesser General Public
     License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
-
-
 package com.stormwyrm.nekoarc.types;
 
 import com.stormwyrm.nekoarc.vm.VirtualMachine;
@@ -33,6 +31,9 @@ public class CodeGen extends ArcObject {
     private int pos, litpos;
     private ArcObject[] genlits;
 
+    /**
+     * Create a new code generator
+     */
     public CodeGen() {
         pos = 0;
         geninst = new byte[32];
@@ -40,6 +41,11 @@ public class CodeGen extends ArcObject {
         genlits = new ArcObject[32];
     }
 
+    /**
+     * Write an instruction
+     * @param b the opcode
+     * @return offset at which the instruction was written
+     */
     private int instwrite(byte b) {
         if (pos >= geninst.length)
             geninst = Arrays.copyOf(geninst, geninst.length * 2);
@@ -49,6 +55,10 @@ public class CodeGen extends ArcObject {
         return(tmppos);
     }
 
+    /**
+     * Load generated code into a virtual machine
+     * @param vm the virtual machine to load into
+     */
     public void load(VirtualMachine vm) {
     //  if (geninst.length > pos)
     //       geninst = Arrays.copyOf(geninst, pos);
@@ -57,6 +67,12 @@ public class CodeGen extends ArcObject {
         vm.load(geninst, genlits);
     }
 
+    /**
+     * Emit an instruction with short (1-byte) parameters
+     * @param op The opcode of the instruction
+     * @param vals The parameters to the instruction
+     * @return the offset at which the instruction was emitted
+     */
     public int emits(byte op, int... vals) {
         int tmppos = instwrite(op);
         for (int val : vals)
@@ -64,6 +80,12 @@ public class CodeGen extends ArcObject {
         return(tmppos);
     }
 
+    /**
+     * Emit an instruction with regular (4-byte) parameters
+     * @param op The opcode of the instruction
+     * @param vals The parameters of the instruction
+     * @return the offset at which the instruction was emitted
+     */
     public int emit(byte op, int... vals) {
         int pos = instwrite(op);
         for (int val : vals) {
@@ -75,6 +97,12 @@ public class CodeGen extends ArcObject {
         return(pos);
     }
 
+    /**
+     * Set a code byte at an offset
+     * @param pos offset in the code
+     * @param val the value to set
+     * @return the position at which the change was made
+     */
     public int setAtPos(int pos, int val) {
         for (int i=pos; i<pos+4; i++) {
             geninst[i] = (byte) (val & 0xff);
@@ -83,14 +111,28 @@ public class CodeGen extends ArcObject {
         return(pos);
     }
 
+    /**
+     * Get a code byte at an offset
+     * @param pos offset in the code
+     * @return the value of the byte at the offset
+     * */
     public byte getAtPos(int pos) {
         return(geninst[pos]);
     }
 
+    /**
+     * Get the current position of the code pointer
+     * @return the position of the code pointer
+     */
     public int pos() {
         return(pos);
     }
 
+    /**
+     * Set the position of the code pointer
+     * @param pos the new code pointer
+     * @return the new code pointer
+     */
     public int setPos(int pos) {
         return(this.pos = pos);
     }
@@ -107,6 +149,11 @@ public class CodeGen extends ArcObject {
         return(newval);
     }
 
+    /**
+     * Add a literal to the literals table
+     * @param lit the literal to add
+     * @return the offset in the literals table at which it was put
+     */
     public int literal(ArcObject lit) {
         if (litpos >= genlits.length)
             genlits = Arrays.copyOf(genlits, genlits.length * 2);
@@ -116,19 +163,36 @@ public class CodeGen extends ArcObject {
         return(tmppos);
     }
 
+    /**
+     * Get current position of literal pointer
+     * @return the literal pointer
+     */
     public int litPos() {
         return(litpos);
     }
 
+    /**
+     * Set the position of the literal pointer
+     * @param litpos new literal position
+     * @return new literal position
+     */
     public int setLitPos(int litpos) {
         return(this.litpos = litpos);
     }
 
+    /**
+     * The type of a CodeGen
+     * @return the type
+     */
     @Override
     public ArcObject type() {
         return(TYPE);
     }
 
+    /**
+     * Convert to string
+     * @return string rep
+     */
     @Override
     public String toString() {
         return("#<CodeGen code: " + pos + " literals: " + litpos + ">");
