c0e04e4d22a1febff4968c51cf8a41cf1b5f291b
Rafael Sevilla <rsevilla@virtela.net>
2018-12-04 13:20:19 +0800

    fix deprecated forms

--- src/test/java/com/stormwyrm/nekoarc/vm/TailRecursionTest.java
+++ src/test/java/com/stormwyrm/nekoarc/vm/TailRecursionTest.java
@@ -12,13 +12,12 @@ public class TailRecursionTest
 	/* This is a simple test for tail recursion. Essentially (fn (x y t) (+ ((afn (acc z) (if (is z 0) acc (self (+ acc x) (- z 1)))) 0 y) t))
 	 * For large enough values of y the stack would overflow with garbage environments unless we do tail recursion properly. */
 	@Test
-	public void test()
-	{
+	public void test() {
 		// env 3 1 0; cont 19; ldi 0; push; lde0 1; push; cls 7; ste0 3; apply 2; push; lde0 2; add; ret;
 		// (note that there are no cont instructions in the second function because all calls are tail calls)
 		// env 2 0 0; lde0 1; push; ldi 0; is; jf xxx; lde0 0; ret; lde0 0; push; lde 1 0; add; push; lde0 1; push; ldi 1; sub; push; lde 1 2; apply 2; ret
 		byte[] inst = {(byte) 0xca, 0x03, 0x01, 0x00,    // env 3 1 0
-				(byte) 0x89, 0x12, 0x00, 0x00, 0x00,        // cont 18
+				(byte) 0x52, 0x12, 0x00, 0x00, 0x00,        // cont 18
 				0x44, 0x00, 0x00, 0x00, 0x00,            // ldi 0
 				0x01,                                    // push
 				0x69, 0x01,                                // lde0 1
@@ -52,17 +51,18 @@ public class TailRecursionTest
 				0x4c, 0x02,                                // apply 2
 				0x0d                                    // ret
 		};
-		ArcThread vm = new ArcThread(14);
+		VirtualMachine vm = new VirtualMachine();
 		vm.load(inst);
-		vm.setargc(3);
-		vm.push(Fixnum.get(1));							// x
-		vm.push(Fixnum.get(50000));						// y
-		vm.push(Fixnum.get(1));							// t
-		vm.setAcc(Nil.NIL);
-		assertTrue(vm.runnable());
-		vm.run();
-		assertFalse(vm.runnable());
-		assertEquals(50001, ((Fixnum)vm.getAcc()).fixnum);
+		ArcThread thr = new ArcThread(vm, 14);
+		thr.setargc(3);
+		thr.push(Fixnum.get(1));							// x
+		thr.push(Fixnum.get(50000));						// y
+		thr.push(Fixnum.get(1));							// t
+		thr.setAcc(Nil.NIL);
+		assertTrue(thr.runnable());
+		thr.main();
+		assertFalse(thr.runnable());
+		assertEquals(50001, ((Fixnum)thr.getAcc()).fixnum);
 	}
 
 }
