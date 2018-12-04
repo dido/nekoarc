e0af0f1c3362ff9a8648c96d6c10f57add538c7a
Rafael Sevilla <rsevilla@virtela.net>
2018-12-04 13:43:30 +0800

    modify to use new code generation idiom

--- src/test/java/com/stormwyrm/nekoarc/functions/CCCTest.java
+++ src/test/java/com/stormwyrm/nekoarc/functions/CCCTest.java
@@ -1,11 +1,26 @@
+/*  Copyright (C) 2018 Rafael R. Sevilla
+
+    This file is part of NekoArc
+
+    NekoArc is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; either version 3 of the
+    License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
 package com.stormwyrm.nekoarc.functions;
 
 import com.stormwyrm.nekoarc.Nil;
 import com.stormwyrm.nekoarc.Op;
-import com.stormwyrm.nekoarc.types.Closure;
-import com.stormwyrm.nekoarc.types.Fixnum;
-import com.stormwyrm.nekoarc.types.Symbol;
-import com.stormwyrm.nekoarc.types.ArcThread;
+import com.stormwyrm.nekoarc.types.*;
+import com.stormwyrm.nekoarc.vm.VirtualMachine;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -13,35 +28,37 @@ import static org.junit.Assert.*;
 public class CCCTest {
     @Test
     public void testCCCsimple() {
-        ArcThread thr = new ArcThread(1024);
-        thr.vm.initSyms();
+        CodeGen cg = new CodeGen();
         // This is basically
         // (ccc (fn (esc) (esc 42) 21))
 
-        Op.ENV.emits(thr, 0, 0, 0);
-        int contpos = Op.CONT.emit(thr, 0);
-        Op.LDL.emit(thr, 1);
-        Op.PUSH.emit(thr);
-        Op.LDG.emit(thr, 0);
-        Op.APPLY.emits(thr, 1);
-        int contdest = Op.RET.emit(thr);
-        thr.vm.cg.patchRelativeBranch(contpos, contdest);
+        Op.ENV.emit(cg, 0, 0, 0);
+        int contpos = Op.CONT.emit(cg, 0);
+        Op.LDL.emit(cg, 1);
+        Op.PUSH.emit(cg);
+        Op.LDG.emit(cg, 0);
+        Op.APPLY.emit(cg, 1);
+        int contdest = Op.RET.emit(cg);
+        cg.patchRelativeBranch(contpos, contdest);
 
         // (fn (esc) (esc 42) 21)
-        int func = Op.ENV.emits(thr, 1, 0, 0);
-        Op.LDI.emit(thr, 42);
-        Op.PUSH.emit(thr);
-        Op.LDE0.emits(thr, 0);
-        Op.APPLY.emits(thr, 1);
-        Op.LDI.emit(thr, 21);
-        Op.RET.emit(thr);
+        int func = Op.ENV.emit(cg, 1, 0, 0);
+        Op.LDI.emit(cg, 42);
+        Op.PUSH.emit(cg);
+        Op.LDE0.emit(cg, 0);
+        Op.APPLY.emit(cg, 1);
+        Op.LDI.emit(cg, 21);
+        Op.RET.emit(cg);
+        cg.literal(Symbol.intern("ccc"));
+        cg.literal(new Closure(Nil.NIL, Fixnum.get(func)));
 
-        thr.vm.cg.literal(Symbol.intern("ccc"));
-        thr.vm.cg.literal(new Closure(Nil.NIL, Fixnum.get(func)));
-        thr.vm.load();
+        VirtualMachine vm = new VirtualMachine(cg);
+        ArcThread thr = new ArcThread(vm);
+        vm.initSyms();
+
         thr.setargc(0);
         assertTrue(thr.runnable());
-        thr.run();
+        thr.main();
         assertFalse(thr.runnable());
         assertEquals(42, ((Fixnum)thr.getAcc()).fixnum);
     }
