fc7a2ffa05f007477219e20911a3d4b2c9bbeb56
Dido Sevilla <dido@stormwyrm.com>
2018-12-05 14:52:27 +0800

    javadoc

--- src/main/java/com/stormwyrm/nekoarc/vm/instruction/APPLY.java
+++ src/main/java/com/stormwyrm/nekoarc/vm/instruction/APPLY.java
@@ -1,16 +1,40 @@
+/*  Copyright (C) 2018 Rafael R. Sevilla
+
+    This file is part of NekoArc
+
+    NekoArc is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; either version 3 of the
+    License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
 package com.stormwyrm.nekoarc.vm.instruction;
 
 import com.stormwyrm.nekoarc.NekoArcException;
 import com.stormwyrm.nekoarc.vm.Instruction;
 import com.stormwyrm.nekoarc.types.ArcThread;
 
-public class APPLY implements Instruction
-{
+/**
+ * The APPLY instruction. Applies the accumulator, given the number of parameters in its argument
+ * on the stack.
+ */
+public class APPLY implements Instruction {
+	/**
+	 * Apply the object in the accumulator to arg
+	 * @param thr the thread executing the APPLY
+	 * @throws NekoArcException if there is an error during the apply
+	 */
 	@Override
-	public void invoke(ArcThread vm) throws NekoArcException
-	{
-		vm.setargc(vm.smallInstArg());
-		vm.getAcc().apply(vm, vm);
+	public void invoke(ArcThread thr) throws NekoArcException {
+		thr.setargc(thr.smallInstArg());
+		thr.getAcc().apply(thr, thr);
 	}
 
 }
