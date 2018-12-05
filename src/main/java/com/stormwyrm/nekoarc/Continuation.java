b453fb56a05d6b87f392835a7c09f2dab419a1a7
Dido Sevilla <dido@stormwyrm.com>
2018-12-05 15:28:47 +0800

    javadoc

--- src/main/java/com/stormwyrm/nekoarc/Continuation.java
+++ src/main/java/com/stormwyrm/nekoarc/Continuation.java
@@ -1,9 +1,33 @@
+/*  Copyright (C) 2018 Rafael R. Sevilla
+
+    This file is part of NekoArc
+
+    NekoArc is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; either version 3 of the
+    License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
 package com.stormwyrm.nekoarc;
 
 import com.stormwyrm.nekoarc.util.Callable;
 import com.stormwyrm.nekoarc.types.ArcThread;
 
-public interface Continuation
-{
-	void restore(ArcThread vm, Callable caller);
+/**
+ * A continuation
+ */
+public interface Continuation {
+	/**
+	 * Restore the continuation
+	 * @param thr The thread restoring the continuation
+	 * @param caller The callable
+	 */
+	void restore(ArcThread thr, Callable caller);
 }
