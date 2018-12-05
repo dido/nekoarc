253dc785ce37c1ad21eed9fc94c6c31eb602a1eb
Dido Sevilla <dido@stormwyrm.com>
2018-12-05 15:31:45 +0800

    javadoc

--- src/main/java/com/stormwyrm/nekoarc/HeapContinuation.java
+++ src/main/java/com/stormwyrm/nekoarc/HeapContinuation.java
@@ -1,3 +1,20 @@
+/*  Copyright (C) 2018 Rafael R. Sevilla
+
+    This file is part of NekoArc
+
+    NekoArc is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; either version 3 of the
+    License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
 package com.stormwyrm.nekoarc;
 
 import com.stormwyrm.nekoarc.types.ArcObject;
@@ -7,15 +24,16 @@ import com.stormwyrm.nekoarc.types.Vector;
 import com.stormwyrm.nekoarc.util.Callable;
 import com.stormwyrm.nekoarc.types.ArcThread;
 
-public class HeapContinuation extends Vector implements Continuation
-{
+/**
+ * Heap-based continuations
+ */
+public class HeapContinuation extends Vector implements Continuation {
 	public static final ArcObject TYPE = Symbol.intern("continuation");
 	private final ArcObject prevcont;
 	private final ArcObject env;
 	private final int ipoffset;
 
-	public HeapContinuation(int size, ArcObject pcont, ArcObject e, int ip)
-	{
+	public HeapContinuation(int size, ArcObject pcont, ArcObject e, int ip) {
 		super(size);
 		prevcont = pcont;
 		env = e;
@@ -24,12 +42,11 @@ public class HeapContinuation extends Vector implements Continuation
 
 	/**
 	 * Move continuation to stack for restoration
-	 * @param thr virtual machine thread
+	 * @param thr thread restoring the continuation
 	 * @param cc callable
 	 */
 	@Override
-	public void restore(ArcThread thr, Callable cc)
-	{
+	public void restore(ArcThread thr, Callable cc) {
 		int svsize = (int)this.len();
 
 		thr.stackcheck(svsize + 4, "stack overflow while restoring heap continuation");
@@ -50,11 +67,10 @@ public class HeapContinuation extends Vector implements Continuation
 		thr.restorecont();
 	}
 
-	public static ArcObject fromStackCont(ArcThread vm, ArcObject sc)
-	{
+	public static ArcObject fromStackCont(ArcThread vm, ArcObject sc) {
 		return(fromStackCont(vm, sc, null));
 	}
-	
+
 	/**
 	 * Create a new heap continuation from a stack-based continuation.
 	 * A stack continuation is a pointer into the stack, such that
@@ -64,40 +80,44 @@ public class HeapContinuation extends Vector implements Continuation
 	 * [cont-4] -> instruction pointer offset
 	 * [cont-(5+n)] -> saved stack elements up to saved base pointer position
 	 * This function copies all of this relevant information to the a new HeapContinuation so it can later be restored
+	 * @param thr The thread whose stack continuation is to be created
+	 * @param sc The stack continuation (offset into the stack)
+	 * @param deepest deepest
+	 * @return the created HeapContinuation
 	 */
-	public static ArcObject fromStackCont(ArcThread vm, ArcObject sc, int[] deepest)
-	{
+	public static ArcObject fromStackCont(ArcThread thr, ArcObject sc, int[] deepest) {
 		if (sc instanceof HeapContinuation || sc.is(Nil.NIL))
 			return(sc);
 		int cc = (int)((Fixnum)sc).fixnum;
 		// Calculate the size of the actual continuation based on the saved base pointer
-		int bp = (int)((Fixnum)vm.stackIndex(cc-3)).fixnum;
+		int bp = (int)((Fixnum)thr.stackIndex(cc-3)).fixnum;
 		int svsize = cc - bp - 4;
 		if (deepest != null && deepest[0] > bp)
 			deepest[0] = bp;
 		// Turn previous continuation into a heap-based one too
-		ArcObject pco = vm.stackIndex(cc-1);
-		pco = fromStackCont(vm, pco, deepest);
-		ArcObject senv = HeapEnv.fromStackEnv(vm, vm.stackIndex(cc-2), deepest);
-		HeapContinuation c = new HeapContinuation(svsize, pco, senv, (int)((Fixnum)vm.stackIndex(cc-4)).fixnum);
+		ArcObject pco = thr.stackIndex(cc-1);
+		pco = fromStackCont(thr, pco, deepest);
+		ArcObject senv = HeapEnv.fromStackEnv(thr, thr.stackIndex(cc-2), deepest);
+		HeapContinuation c = new HeapContinuation(svsize, pco, senv,
+				(int)((Fixnum)thr.stackIndex(cc-4)).fixnum);
 
 		for (int i=0; i<svsize; i++)
-			c.setIndex(i, vm.stackIndex(bp + i));
+			c.setIndex(i, thr.stackIndex(bp + i));
 		return(c);
 	}
 
 	@Override
-	public int requiredArgs()
-	{
+	public int requiredArgs() {
 		return(1);
 	}
 
-	/** The application of a continuation -- this will set itself as the current continuation,
-	 *  ready to be restored just as the invokethread terminates. */
+	/**
+	 * The application of a continuation -- this will set itself as the current continuation,
+	 * ready to be restored just as the invokethread terminates.
+	 */
 	@Override
-	public ArcObject invoke(InvokeThread thr)
-	{
-		thr.thr.setCont(this);
-		return(thr.getenv(0, 0));
+	public ArcObject invoke(InvokeThread ithr) {
+		ithr.thr.setCont(this);
+		return(ithr.getenv(0, 0));
 	}
 }
