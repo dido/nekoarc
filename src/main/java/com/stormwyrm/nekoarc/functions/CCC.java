acd74694ee55e52815d5dbeb580e8465c942cc00
Dido Sevilla <dido@stormwyrm.com>
2018-12-05 15:21:56 +0800

    Pass a StopContinuation if ccc is called with no current continuation

--- src/main/java/com/stormwyrm/nekoarc/functions/CCC.java
+++ src/main/java/com/stormwyrm/nekoarc/functions/CCC.java
@@ -1,29 +1,55 @@
+/*  Copyright (C) 2018 Rafael R. Sevilla
+
+    This file is part of NekoArc
+
+    NekoArc is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; either version 3 of the
+    License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
 package com.stormwyrm.nekoarc.functions;
 
-import com.stormwyrm.nekoarc.HeapContinuation;
-import com.stormwyrm.nekoarc.InvokeThread;
-import com.stormwyrm.nekoarc.NekoArcException;
+import com.stormwyrm.nekoarc.*;
 import com.stormwyrm.nekoarc.types.ArcObject;
 import com.stormwyrm.nekoarc.types.Fixnum;
 
-public class CCC extends Builtin
-{
+/**
+ * Builtin function ccc - Call with Current Continuation. Accepts one parameter, which is a closure.
+ */
+public class CCC extends Builtin {
 	private static final CCC INSTANCE = new CCC();
-	private CCC()
-	{
+	private CCC() {
 		super("ccc", 1);
 	}
 
+	/**
+	 * Get the instance of this.
+	 * @return
+	 */
 	public static Builtin getInstance() {
 		return(INSTANCE);
 	}
 
+	/**
+	 * Invoke ccc.
+	 * @param thr The thread executing ccc
+	 * @return The return value of the closure passed to it.
+	 */
 	@Override
-	public ArcObject invoke(InvokeThread thr)
-	{
+	public ArcObject invoke(InvokeThread thr) {
 		ArcObject continuation = thr.thr.getCont();
 		if (continuation instanceof Fixnum)
 			continuation = HeapContinuation.fromStackCont(thr.thr, continuation);
+		if (Nil.NIL.is(continuation))
+			continuation = new StopContinuation();
 		if (!(continuation instanceof HeapContinuation))
 			throw new NekoArcException("Invalid continuation type " + continuation.type().toString());
 		return(thr.apply(thr.getenv(0,  0), continuation));
