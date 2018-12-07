b8530c5dbec695dd269ef49a0904b8f04815501f
Rafael Sevilla <rsevilla@virtela.net>
2018-12-07 11:23:26 +0800

    remove deprecated usage of value

--- src/main/java/com/stormwyrm/nekoarc/functions/io/UngetC.java
+++ src/main/java/com/stormwyrm/nekoarc/functions/io/UngetC.java
@@ -22,7 +22,7 @@ public class UngetC extends Builtin {
     public ArcObject invoke(InvokeThread ithr) {
         Rune r = (Rune) ithr.getenv(0, 0);
         ArcObject f = ithr.getenv(0, 1).car();
-        InputPort fp = (f instanceof InputPort) ? ((InputPort)f) : ((InputPort) ithr.thr.value((Symbol)Symbol.intern("stdin")));
+        InputPort fp = (f instanceof InputPort) ? ((InputPort)f) : ((InputPort) ithr.thr.vm.value((Symbol)Symbol.intern("stdin")));
         return(fp.ungetc(r));
     }
 }
