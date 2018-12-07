ab78f434dcc5ad0c05bd78b2882b817616177e56
Rafael Sevilla <rsevilla@virtela.net>
2018-12-07 10:35:57 +0800

    remove deprecated use of bind from the thread

--- src/main/java/com/stormwyrm/nekoarc/vm/instruction/STG.java
+++ src/main/java/com/stormwyrm/nekoarc/vm/instruction/STG.java
@@ -1,3 +1,20 @@
+/*  Copyright (C) 2018 Rafael R. Sevilla
+
+    This file is part of NekoArc
+
+    NekoArc is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; either version 3 of the
+    License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
 package com.stormwyrm.nekoarc.vm.instruction;
 
 import com.stormwyrm.nekoarc.NekoArcException;
@@ -5,15 +22,21 @@ import com.stormwyrm.nekoarc.types.Symbol;
 import com.stormwyrm.nekoarc.vm.Instruction;
 import com.stormwyrm.nekoarc.types.ArcThread;
 
-public class STG implements Instruction
-{
+/**
+ * STG (Store Global) instruction
+ */
+public class STG implements Instruction {
 
+	/**
+	 * STG - Store Global. Bind the value at top of stack to the symbol in the argument.
+	 * @param thr The thread executing the STG instruction
+	 * @throws NekoArcException on error
+	 */
 	@Override
-	public void invoke(ArcThread vm) throws NekoArcException
-	{
-		int offset = vm.instArg();
-		Symbol sym = (Symbol)vm.literal(offset);
-		vm.bind(sym, vm.getAcc());
+	public void invoke(ArcThread thr) throws NekoArcException {
+		int offset = thr.instArg();
+		Symbol sym = (Symbol)thr.literal(offset);
+		thr.vm.bind(sym, thr.getAcc());
 	}
 
 }
