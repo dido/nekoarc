b0b53f8ab0732781a6b09bfb5255c50baf661d89
Dido Sevilla <dido@stormwyrm.com>
2018-12-09 14:03:10 +0800

    LDIP (load immediate and push)

---
+++ src/main/java/com/stormwyrm/nekoarc/vm/instruction/LDIP.java
@@ -0,0 +1,37 @@
+/*  Copyright (C) 2018 Rafael R. Sevilla
+
+    This file is part of NekoArc
+
+    NekoArc is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; either version 3 of the
+    License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+package com.stormwyrm.nekoarc.vm.instruction;
+
+import com.stormwyrm.nekoarc.types.ArcThread;
+import com.stormwyrm.nekoarc.types.Fixnum;
+import com.stormwyrm.nekoarc.vm.Instruction;
+
+/**
+ * LDIP - Load Immediate and Push
+ */
+public class LDIP implements Instruction {
+    /**
+     * Load immediate from argument and push
+     * @param thr The thread executing the instruction
+     */
+    @Override
+    public void invoke(ArcThread thr) {
+        long value = thr.instArg();
+        thr.push(thr.setAcc(Fixnum.get(value)));
+    }
+}
