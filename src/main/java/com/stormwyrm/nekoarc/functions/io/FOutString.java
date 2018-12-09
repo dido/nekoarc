d15e98a1c6e8dbdcee7fbe4d45ceb1e4b4217b17
Dido Sevilla <dido@stormwyrm.com>
2018-12-09 13:32:27 +0800

    outstring function

---
+++ src/main/java/com/stormwyrm/nekoarc/functions/io/FOutString.java
@@ -0,0 +1,44 @@
+/*  Copyright (C) 2018 Rafael R. Sevilla
+
+    This file is part of NekoArc
+
+    NekoArc is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; either version 3 of the
+    License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+package com.stormwyrm.nekoarc.functions.io;
+
+import com.stormwyrm.nekoarc.InvokeThread;
+import com.stormwyrm.nekoarc.Unbound;
+import com.stormwyrm.nekoarc.functions.Builtin;
+import com.stormwyrm.nekoarc.types.ArcObject;
+import com.stormwyrm.nekoarc.types.OutString;
+import com.stormwyrm.nekoarc.types.Symbol;
+
+public class FOutString extends Builtin  {
+    private static final FOutString INSTANCE = new FOutString();
+
+    private FOutString() {
+        super("outstring", 0, 1, 0, false);
+    }
+
+    public static Builtin getInstance() {
+        return(INSTANCE);
+    }
+
+    @Override
+    public ArcObject invoke(InvokeThread ithr) {
+        if (Unbound.UNBOUND.is(ithr.getenv(0)) || !(ithr.getenv(0) instanceof Symbol))
+            return(new OutString());
+        return(new OutString(ithr.getenv(0).toString()));
+    }
+}
