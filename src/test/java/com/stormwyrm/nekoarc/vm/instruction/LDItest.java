8dc71814c0cb449fe541b8809a06b50181d98126
Dido Sevilla <dido@stormwyrm.com>
2018-12-09 13:56:57 +0800

    update test and remove deprecated

--- src/test/java/com/stormwyrm/nekoarc/vm/instruction/LDItest.java
+++ src/test/java/com/stormwyrm/nekoarc/vm/instruction/LDItest.java
@@ -1,28 +1,54 @@
+/*  Copyright (C) 2018 Rafael R. Sevilla
+
+    This file is part of NekoArc
+
+    NekoArc is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; either version 3 of the
+    License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
 package com.stormwyrm.nekoarc.vm.instruction;
 
 import static org.junit.Assert.*;
 
 import com.stormwyrm.nekoarc.NekoArcException;
 import com.stormwyrm.nekoarc.Nil;
+import com.stormwyrm.nekoarc.Op;
+import com.stormwyrm.nekoarc.types.CodeGen;
 import com.stormwyrm.nekoarc.types.Fixnum;
 import com.stormwyrm.nekoarc.types.ArcThread;
+import com.stormwyrm.nekoarc.vm.VirtualMachine;
 import org.junit.Test;
 
-public class LDItest
-{
+public class LDItest {
 	@Test
-	public void testLDI() throws NekoArcException
-	{
+	public void testLDI() throws NekoArcException {
 		// ldi 1; hlt
         byte[] inst = {0x44, 0x01, 0x00, 0x00, 0x00, 0x14};
-		ArcThread vm = new ArcThread(1024);
-		vm.load(inst);
-		vm.setAcc(Nil.NIL);
-		assertTrue(vm.runnable());
-		vm.run();
-		assertFalse(vm.runnable());
-		assertEquals(Fixnum.get(1), vm.getAcc());
-		assertEquals(6, vm.getIP());
+		CodeGen cg = new CodeGen();
+		Op.LDI.emit(cg, 1);
+		Op.HLT.emit(cg);
+
+		for (int i=0; i<inst.length; i++)
+			assertEquals(inst[i], cg.getAtPos(i));
+
+		VirtualMachine vm = new VirtualMachine(cg);
+		ArcThread thr = new ArcThread(vm, 1024);
+		vm.load();
+		thr.setAcc(Nil.NIL);
+		assertTrue(thr.runnable());
+		thr.run();
+		assertFalse(thr.runnable());
+		assertEquals(Fixnum.get(1), thr.getAcc());
+		assertEquals(6, thr.getIP());
 		
 	}
 }
