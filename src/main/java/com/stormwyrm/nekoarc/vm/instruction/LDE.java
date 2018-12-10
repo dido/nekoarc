018551161bf2e330018c0dd4710a2e06b364f512
Rafael Sevilla <rsevilla@virtela.net>
2018-12-10 15:35:16 +0800

    javadoc

--- src/main/java/com/stormwyrm/nekoarc/vm/instruction/LDE.java
+++ src/main/java/com/stormwyrm/nekoarc/vm/instruction/LDE.java
@@ -1,14 +1,37 @@
+/*  Copyright (C) 2018 Rafael R. Sevilla
+
+    This file is part of NekoArc
+
+    NekoArc is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; either version 3 of the
+    License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
 package com.stormwyrm.nekoarc.vm.instruction;
 
 import com.stormwyrm.nekoarc.NekoArcException;
 import com.stormwyrm.nekoarc.vm.Instruction;
 import com.stormwyrm.nekoarc.types.ArcThread;
 
-public class LDE implements Instruction
-{
+/**
+ * LDE - Load from Environment
+ */
+public class LDE implements Instruction {
+	/**
+	 * Load a value from the environment specified by the two parameters
+	 * @param thr The thread executing the instruction
+	 * @throws NekoArcException on error
+	 */
 	@Override
-	public void invoke(ArcThread thr) throws NekoArcException
-	{
+	public void invoke(ArcThread thr) throws NekoArcException {
 		thr.setAcc(thr.getenv(thr.smallInstArg() & 0xff, thr.smallInstArg() & 0xff));
 	}
 
