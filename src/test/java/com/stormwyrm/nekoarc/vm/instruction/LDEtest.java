f13438dea5522b45b3a5eb809c6d0bf216ec2ff0
Rafael Sevilla <rsevilla@virtela.net>
2018-12-11 15:27:40 +0800

    clean up tests and remove deprecated stuff

--- src/test/java/com/stormwyrm/nekoarc/vm/instruction/LDEtest.java
+++ src/test/java/com/stormwyrm/nekoarc/vm/instruction/LDEtest.java
@@ -1,19 +1,38 @@
+/*  Copyright (C) 2018 Rafael R. Sevilla
+
+    This file is part of NekoArc
+
+    NekoArc is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; either version 3 of the
+    License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
 package com.stormwyrm.nekoarc.vm.instruction;
 
 import static org.junit.Assert.*;
 
 import com.stormwyrm.nekoarc.Nil;
+import com.stormwyrm.nekoarc.Op;
 import com.stormwyrm.nekoarc.Unbound;
+import com.stormwyrm.nekoarc.types.ArcObject;
+import com.stormwyrm.nekoarc.types.CodeGen;
 import com.stormwyrm.nekoarc.types.Fixnum;
 import com.stormwyrm.nekoarc.types.ArcThread;
+import com.stormwyrm.nekoarc.vm.VirtualMachine;
 import org.junit.Test;
 
-public class LDEtest
-{
+public class LDEtest {
 	@Test
-	public void test0()
-	{
-		// ldi 1; push; ldi 2; push; ldi 3; push; env 3 2; lde 0 0; hlt;
+	public void test0() {
+		// ldi 1; push; ldi 2; push; ldi 3; push; env 3 1 1; lde 0 0; hlt;
         byte[] inst = {0x44, 0x01, 0x00, 0x00, 0x00,
                 0x01,
                 0x44, 0x02, 0x00, 0x00, 0x00,
@@ -23,21 +42,35 @@ public class LDEtest
                 (byte) 0xca, 0x03, 0x01, 0x01,
                 (byte) 0x87, 0x00, 0x00,
                 0x14};
-		ArcThread vm = new ArcThread(1024);
-		vm.load(inst);
-		vm.setargc(3);
-		vm.setAcc(Nil.NIL);
-		assertTrue(vm.runnable());
-		vm.run();
-		assertFalse(vm.runnable());
-		assertEquals(Fixnum.get(1), vm.getAcc());
-		assertEquals(26, vm.getIP());
+        CodeGen cg = new CodeGen();
+        Op.LDI.emit(cg, 1);
+        Op.PUSH.emit(cg);
+        Op.LDI.emit(cg, 2);
+        Op.PUSH.emit(cg);
+        Op.LDI.emit(cg, 3);
+        Op.PUSH.emit(cg);
+        Op.ENV.emit(cg, 3, 1, 1);
+        Op.LDE.emit(cg, 0, 0);
+        Op.HLT.emit(cg);
+        assertEquals(cg.pos(), inst.length);
+		for (int i=0; i<inst.length; i++)
+			assertEquals(inst[i], cg.getAtPos(i));
+
+		VirtualMachine vm = new VirtualMachine(cg);
+		vm.load();
+		ArcThread thr = new ArcThread(vm);
+		thr.setargc(3);
+		thr.setAcc(Nil.NIL);
+		assertTrue(thr.runnable());
+		thr.run();
+		assertFalse(thr.runnable());
+		assertEquals(Fixnum.get(1), thr.getAcc());
+		assertEquals(26, thr.getIP());
 	}
 
 	@Test
-	public void test1()
-	{
-		// ldi 1; push; ldi 2; push; ldi 3; push; env 3 2; lde 0 0; hlt;
+	public void test1() {
+		// ldi 1; push; ldi 2; push; ldi 3; push; env 3 2; lde 0 1; hlt;
         byte[] inst = {0x44, 0x01, 0x00, 0x00, 0x00,
                 0x01,
                 0x44, 0x02, 0x00, 0x00, 0x00,
@@ -47,20 +80,33 @@ public class LDEtest
                 (byte) 0xca, 0x03, 0x01, 0x01,
                 (byte) 0x87, 0x00, 0x01,
                 0x14};
-		ArcThread vm = new ArcThread(1024);
-		vm.load(inst);
-		vm.setargc(3);
-		vm.setAcc(Nil.NIL);
-		assertTrue(vm.runnable());
-		vm.run();
-		assertFalse(vm.runnable());
-		assertEquals(Fixnum.get(2), vm.getAcc());
-		assertEquals(26, vm.getIP());
+		CodeGen cg = new CodeGen();
+		Op.LDI.emit(cg, 1);
+		Op.PUSH.emit(cg);
+		Op.LDI.emit(cg, 2);
+		Op.PUSH.emit(cg);
+		Op.LDI.emit(cg, 3);
+		Op.PUSH.emit(cg);
+		Op.ENV.emit(cg, 3, 1, 1);
+		Op.LDE.emit(cg, 0, 1);
+		Op.HLT.emit(cg);
+		assertEquals(cg.pos(), inst.length);
+		for (int i=0; i<inst.length; i++)
+			assertEquals(inst[i], cg.getAtPos(i));
+		VirtualMachine vm = new VirtualMachine(cg);
+		vm.load();
+		ArcThread thr = new ArcThread(vm);
+		thr.setargc(3);
+		thr.setAcc(Nil.NIL);
+		assertTrue(thr.runnable());
+		thr.run();
+		assertFalse(thr.runnable());
+		assertEquals(Fixnum.get(2), thr.getAcc());
+		assertEquals(26, thr.getIP());
 	}
 
 	@Test
-	public void test2()
-	{
+	public void test2() {
 		// ldi 1; push; ldi 2; push; ldi 3; push; env 3 2; lde 0 0; hlt;
         byte[] inst = {0x44, 0x01, 0x00, 0x00, 0x00,
                 0x01,
@@ -71,20 +117,70 @@ public class LDEtest
                 (byte) 0xca, 0x03, 0x01, 0x01,
                 (byte) 0x87, 0x00, 0x02,
                 0x14};
-		ArcThread vm = new ArcThread(1024);
-		vm.load(inst);
-		vm.setargc(3);
-		vm.setAcc(Nil.NIL);
-		assertTrue(vm.runnable());
-		vm.run();
-		assertFalse(vm.runnable());
-		assertEquals(Fixnum.get(3), vm.getAcc());
-		assertEquals(26, vm.getIP());
+		CodeGen cg = new CodeGen();
+		Op.LDI.emit(cg, 1);
+		Op.PUSH.emit(cg);
+		Op.LDI.emit(cg, 2);
+		Op.PUSH.emit(cg);
+		Op.LDI.emit(cg, 3);
+		Op.PUSH.emit(cg);
+		Op.ENV.emit(cg, 3, 1, 1);
+		Op.LDE.emit(cg, 0, 2);
+		Op.HLT.emit(cg);
+		assertEquals(cg.pos(), inst.length);
+		for (int i=0; i<inst.length; i++)
+			assertEquals(inst[i], cg.getAtPos(i));
+		VirtualMachine vm = new VirtualMachine(cg);
+		vm.load();
+		ArcThread thr = new ArcThread(vm);
+		thr.setargc(3);
+		thr.setAcc(Nil.NIL);
+		assertTrue(thr.runnable());
+		thr.run();
+		assertFalse(thr.runnable());
+		assertEquals(Fixnum.get(3), thr.getAcc());
+		assertEquals(26, thr.getIP());
 	}
 
 	@Test
-	public void test3()
-	{
+	public void test3() {
+		// ldi 1; push; ldi 2; push; ldi 3; push; env 3 1 1; lde 0 0; hlt;
+        byte[] inst = {0x44, 0x01, 0x00, 0x00, 0x00,
+                0x01,
+                0x44, 0x02, 0x00, 0x00, 0x00,
+                0x01,
+                0x44, 0x03, 0x00, 0x00, 0x00,
+                0x01,
+                (byte) 0xca, 0x03, 0x01, 0x01,
+                (byte) 0x87, 0x00, 0x03,
+                0x14};
+		CodeGen cg = new CodeGen();
+		Op.LDI.emit(cg, 1);
+		Op.PUSH.emit(cg);
+		Op.LDI.emit(cg, 2);
+		Op.PUSH.emit(cg);
+		Op.LDI.emit(cg, 3);
+		Op.PUSH.emit(cg);
+		Op.ENV.emit(cg, 3, 1, 1);
+		Op.LDE.emit(cg, 0, 3);
+		Op.HLT.emit(cg);
+		assertEquals(cg.pos(), inst.length);
+		for (int i=0; i<inst.length; i++)
+			assertEquals(inst[i], cg.getAtPos(i));
+		VirtualMachine vm = new VirtualMachine(cg);
+		vm.load();
+		ArcThread thr = new ArcThread(vm);
+		thr.setargc(3);
+		thr.setAcc(Nil.NIL);
+		assertTrue(thr.runnable());
+		thr.run();
+		assertFalse(thr.runnable());
+		assertTrue(thr.getAcc().is(Unbound.UNBOUND));
+		assertEquals(26, thr.getIP());
+	}
+
+	@Test
+	public void test4() {
 		// ldi 1; push; ldi 2; push; ldi 3; push; env 3 2; lde 0 0; hlt;
         byte[] inst = {0x44, 0x01, 0x00, 0x00, 0x00,
                 0x01,
@@ -93,300 +189,130 @@ public class LDEtest
                 0x44, 0x03, 0x00, 0x00, 0x00,
                 0x01,
                 (byte) 0xca, 0x03, 0x01, 0x01,
-                (byte) 0x87, 0x00, 0x03,
+                (byte) 0x87, 0x00, 0x04,
                 0x14};
-		ArcThread vm = new ArcThread(1024);
-		vm.load(inst);
-		vm.setargc(3);
-		vm.setAcc(Nil.NIL);
-		assertTrue(vm.runnable());
-		vm.run();
-		assertFalse(vm.runnable());
-		assertTrue(vm.getAcc().is(Unbound.UNBOUND));
-		assertEquals(26, vm.getIP());
+		CodeGen cg = new CodeGen();
+		Op.LDI.emit(cg, 1);
+		Op.PUSH.emit(cg);
+		Op.LDI.emit(cg, 2);
+		Op.PUSH.emit(cg);
+		Op.LDI.emit(cg, 3);
+		Op.PUSH.emit(cg);
+		Op.ENV.emit(cg, 3, 1, 1);
+		Op.LDE.emit(cg, 0, 4);
+		Op.HLT.emit(cg);
+		assertEquals(cg.pos(), inst.length);
+		for (int i=0; i<inst.length; i++)
+			assertEquals(inst[i], cg.getAtPos(i));
+		VirtualMachine vm = new VirtualMachine(cg);
+		vm.load();
+		ArcThread thr = new ArcThread(vm);
+		thr.setargc(3);
+		thr.setAcc(Nil.NIL);
+		assertTrue(thr.runnable());
+		thr.run();
+		assertFalse(thr.runnable());
+		assertTrue(thr.getAcc().is(Unbound.UNBOUND));
+		assertEquals(26, thr.getIP());
+	}
+
+	private void testtmpl(int depth, int idx, ArcObject expected) {
+		// ldi 1; push; ldi 2; push; ldi 3; push; env 3 1 1; ldi 4; push; env 1 0 2; lde depth idx; hlt;
+		byte[] inst = {0x44, 0x01, 0x00, 0x00, 0x00,
+				0x01,
+				0x44, 0x02, 0x00, 0x00, 0x00,
+				0x01,
+				0x44, 0x03, 0x00, 0x00, 0x00,
+				0x01,
+				(byte) 0xca, 0x03, 0x01, 0x01,
+				0x44, 0x04, 0x00, 0x00, 0x00,
+				0x01,
+				0x44, 0x05, 0x00, 0x00, 0x00,
+				0x01,
+				0x44, 0x06, 0x00, 0x00, 0x00,
+				0x01,
+				(byte) 0xca, 0x01, 0x00, 0x03,
+				(byte) 0x87, (byte)depth, (byte)idx,
+				0x14};
+		CodeGen cg = new CodeGen();
+		Op.LDI.emit(cg, 1);
+		Op.PUSH.emit(cg);
+		Op.LDI.emit(cg, 2);
+		Op.PUSH.emit(cg);
+		Op.LDI.emit(cg, 3);
+		Op.PUSH.emit(cg);
+		Op.ENV.emit(cg, 3, 1, 1);
+		Op.LDI.emit(cg, 4);
+		Op.PUSH.emit(cg);
+		Op.LDI.emit(cg, 5);
+		Op.PUSH.emit(cg);
+		Op.LDI.emit(cg, 6);
+		Op.PUSH.emit(cg);
+		Op.ENV.emit(cg, 1, 0, 3);
+		Op.LDE.emit(cg, depth, idx);
+		Op.HLT.emit(cg);
+		assertEquals(cg.pos(), inst.length);
+		for (int i=0; i<inst.length; i++)
+			assertEquals(inst[i], cg.getAtPos(i));
+		VirtualMachine vm = new VirtualMachine(cg);
+		vm.load();
+		ArcThread thr = new ArcThread(vm);
+		thr.setargc(3);
+		thr.setAcc(Nil.NIL);
+		assertTrue(thr.runnable());
+		thr.run();
+		assertFalse(thr.runnable());
+		assertTrue(expected.is(thr.getAcc()));
+		assertEquals(48, thr.getIP());
+	}
+
+	private void testtmpl(int depth, int idx, int expected) {
+		testtmpl(depth, idx, Fixnum.get(expected));
 	}
 
 	@Test
-	public void test4()
-	{
-		// ldi 1; push; ldi 2; push; ldi 3; push; env 3 2; lde 0 0; hlt;
-        byte[] inst = {0x44, 0x01, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x02, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x03, 0x00, 0x00, 0x00,
-                0x01,
-                (byte) 0xca, 0x03, 0x01, 0x01,
-                (byte) 0x87, 0x00, 0x03,
-                0x14};
-		ArcThread vm = new ArcThread(1024);
-		vm.load(inst);
-		vm.setargc(3);
-		vm.setAcc(Nil.NIL);
-		assertTrue(vm.runnable());
-		vm.run();
-		assertFalse(vm.runnable());
-		assertTrue(vm.getAcc().is(Unbound.UNBOUND));
-		assertEquals(26, vm.getIP());
+	public void test1_0() {
+		testtmpl(1,0, 1);
 	}
 
 	@Test
-	public void test1_0()
-	{
-		// ldi 1; push; ldi 2; push; ldi 3; push; env 3 1 1; ldi 4; push; env 1 0 2; lde 1 0; hlt;
-        byte[] inst = {0x44, 0x01, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x02, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x03, 0x00, 0x00, 0x00,
-                0x01,
-                (byte) 0xca, 0x03, 0x01, 0x01,
-                0x44, 0x04, 0x00, 0x00, 0x00,
-                0x01,
-                (byte) 0xca, 0x01, 0x00, 0x02,
-                (byte) 0x87, 0x01, 0x00,
-                0x14};
-		ArcThread vm = new ArcThread(1024);
-		vm.load(inst);
-		vm.setargc(3);
-		vm.setAcc(Nil.NIL);
-		assertTrue(vm.runnable());
-		vm.run();
-		assertFalse(vm.runnable());
-		assertEquals(Fixnum.get(1), vm.getAcc());
-		assertEquals(36, vm.getIP());
+	public void test1_1() {
+		testtmpl(1,1, 2);
 	}
 
 	@Test
-	public void test1_1()
-	{
-		// ldi 1; push; ldi 2; push; ldi 3; push; env 3 1 1; ldi 4; push; env 1 0 2; lde 1 1; hlt;
-        byte[] inst = {0x44, 0x01, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x02, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x03, 0x00, 0x00, 0x00,
-                0x01,
-                (byte) 0xca, 0x03, 0x01, 0x01,
-                0x44, 0x04, 0x00, 0x00, 0x00,
-                0x01,
-                (byte) 0xca, 0x01, 0x00, 0x02,
-                (byte) 0x87, 0x01, 0x01,
-                0x14};
-		ArcThread vm = new ArcThread(1024);
-		vm.load(inst);
-		vm.setargc(3);
-		vm.setAcc(Nil.NIL);
-		assertTrue(vm.runnable());
-		vm.run();
-		assertFalse(vm.runnable());
-		assertEquals(Fixnum.get(2), vm.getAcc());
-		assertEquals(36, vm.getIP());
+	public void test1_2() {
+		testtmpl(1, 2, 3);
 	}
 
 	@Test
-	public void test1_2()
-	{
-		// ldi 1; push; ldi 2; push; ldi 3; push; env 3 1 1; ldi 4; push; env 1 0 2; lde 1 3; hlt;
-        byte[] inst = {0x44, 0x01, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x02, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x03, 0x00, 0x00, 0x00,
-                0x01,
-                (byte) 0xca, 0x03, 0x01, 0x01,
-                0x44, 0x04, 0x00, 0x00, 0x00,
-                0x01,
-                (byte) 0xca, 0x01, 0x00, 0x02,
-                (byte) 0x87, 0x01, 0x02,
-                0x14};
-		ArcThread vm = new ArcThread(1024);
-		vm.load(inst);
-		vm.setargc(3);
-		vm.setAcc(Nil.NIL);
-		assertTrue(vm.runnable());
-		vm.run();
-		assertFalse(vm.runnable());
-		assertEquals(Fixnum.get(3), vm.getAcc());
-		assertEquals(36, vm.getIP());
+	public void test1_3() {
+		testtmpl(1,3, Unbound.UNBOUND);
 	}
 
 	@Test
-	public void test1_3()
-	{
-		// ldi 1; push; ldi 2; push; ldi 3; push; env 3 1 1; ldi 4; push; env 1 0 2; lde 1 2; hlt;
-        byte[] inst = {0x44, 0x01, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x02, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x03, 0x00, 0x00, 0x00,
-                0x01,
-                (byte) 0xca, 0x03, 0x01, 0x01,
-                0x44, 0x04, 0x00, 0x00, 0x00,
-                0x01,
-                (byte) 0xca, 0x01, 0x00, 0x02,
-                (byte) 0x87, 0x01, 0x03,
-                0x14};
-		ArcThread vm = new ArcThread(1024);
-		vm.load(inst);
-		vm.setargc(3);
-		vm.setAcc(Nil.NIL);
-		assertTrue(vm.runnable());
-		vm.run();
-		assertFalse(vm.runnable());
-		assertTrue(vm.getAcc().is(Unbound.UNBOUND));
-		assertEquals(36, vm.getIP());
+	public void test1_4() {
+		testtmpl(1,4, Unbound.UNBOUND);
 	}
 
 	@Test
-	public void test1_4()
-	{
-		// ldi 1; push; ldi 2; push; ldi 3; push; env 3 1 1; ldi 4; push; env 1 0 2; lde 1 4; hlt;
-        byte[] inst = {0x44, 0x01, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x02, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x03, 0x00, 0x00, 0x00,
-                0x01,
-                (byte) 0xca, 0x03, 0x01, 0x01,
-                0x44, 0x04, 0x00, 0x00, 0x00,
-                0x01,
-                (byte) 0xca, 0x01, 0x00, 0x02,
-                (byte) 0x87, 0x01, 0x04,
-                0x14};
-		ArcThread vm = new ArcThread(1024);
-		vm.load(inst);
-		vm.setargc(3);
-		vm.setAcc(Nil.NIL);
-		assertTrue(vm.runnable());
-		vm.run();
-		assertFalse(vm.runnable());
-		assertTrue(vm.getAcc().is(Unbound.UNBOUND));
-		assertEquals(36, vm.getIP());
+	public void test0_0() {
+		testtmpl(0,0, 4);
 	}
 
 	@Test
-	public void test0_0()
-	{
-		// ldi 1; push; ldi 2; push; ldi 3; push; env 3 1 1; ldi 4; push; ldi 5; push; ldi 6; push; env 1 0 3; lde 0 0; hlt;
-        byte[] inst = {0x44, 0x01, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x02, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x03, 0x00, 0x00, 0x00,
-                0x01,
-                (byte) 0xca, 0x03, 0x01, 0x01,
-                0x44, 0x04, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x05, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x06, 0x00, 0x00, 0x00,
-                0x01,
-                (byte) 0xca, 0x01, 0x00, 0x03,
-                (byte) 0x87, 0x00, 0x00,
-                0x14};
-		ArcThread vm = new ArcThread(1024);
-		vm.load(inst);
-		vm.setargc(3);
-		vm.setAcc(Nil.NIL);
-		assertTrue(vm.runnable());
-		vm.run();
-		assertFalse(vm.runnable());
-		assertEquals(Fixnum.get(4), vm.getAcc());
-		assertEquals(48, vm.getIP());
+	public void test0_1() {
+		testtmpl(0,1, 5);
 	}
 
 	@Test
-	public void test0_1()
-	{
-		// ldi 1; push; ldi 2; push; ldi 3; push; env 3 1 1; ldi 4; push; ldi 5; push; ldi 6; push; env 1 0 3; lde 0 1; hlt;
-        byte[] inst = {0x44, 0x01, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x02, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x03, 0x00, 0x00, 0x00,
-                0x01,
-                (byte) 0xca, 0x03, 0x01, 0x01,
-                0x44, 0x04, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x05, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x06, 0x00, 0x00, 0x00,
-                0x01,
-                (byte) 0xca, 0x01, 0x00, 0x03,
-                (byte) 0x87, 0x00, 0x01,
-                0x14};
-		ArcThread vm = new ArcThread(1024);
-		vm.load(inst);
-		vm.setargc(3);
-		vm.setAcc(Nil.NIL);
-		assertTrue(vm.runnable());
-		vm.run();
-		assertFalse(vm.runnable());
-		assertEquals(Fixnum.get(5), vm.getAcc());
-		assertEquals(48, vm.getIP());
+	public void test0_2() {
+		testtmpl(0,2, 6);
 	}
 
 	@Test
-	public void test0_2()
-	{
-		// ldi 1; push; ldi 2; push; ldi 3; push; env 3 1 1; ldi 4; push; ldi 5; push; ldi 6; push; env 1 0 3; lde 0 1; hlt;
-        byte[] inst = {0x44, 0x01, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x02, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x03, 0x00, 0x00, 0x00,
-                0x01,
-                (byte) 0xca, 0x03, 0x01, 0x01,
-                0x44, 0x04, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x05, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x06, 0x00, 0x00, 0x00,
-                0x01,
-                (byte) 0xca, 0x01, 0x00, 0x03,
-                (byte) 0x87, 0x00, 0x02,
-                0x14};
-		ArcThread vm = new ArcThread(1024);
-		vm.load(inst);
-		vm.setargc(3);
-		vm.setAcc(Nil.NIL);
-		assertTrue(vm.runnable());
-		vm.run();
-		assertFalse(vm.runnable());
-		assertEquals(Fixnum.get(6), vm.getAcc());
-		assertEquals(48, vm.getIP());
-	}
-
-	@Test
-	public void test0_3()
-	{
-		// ldi 1; push; ldi 2; push; ldi 3; push; env 3 1 1; ldi 4; push; ldi 5; push; ldi 6; push; env 1 0 3; lde 0 1; hlt;
-        byte[] inst = {0x44, 0x01, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x02, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x03, 0x00, 0x00, 0x00,
-                0x01,
-                (byte) 0xca, 0x03, 0x01, 0x01,
-                0x44, 0x04, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x05, 0x00, 0x00, 0x00,
-                0x01,
-                0x44, 0x06, 0x00, 0x00, 0x00,
-                0x01,
-                (byte) 0xca, 0x01, 0x00, 0x03,
-                (byte) 0x87, 0x00, 0x03,
-                0x14};
-		ArcThread vm = new ArcThread(1024);
-		vm.load(inst);
-		vm.setargc(3);
-		vm.setAcc(Nil.NIL);
-		assertTrue(vm.runnable());
-		vm.run();
-		assertFalse(vm.runnable());
-		assertTrue(vm.getAcc().is(Unbound.UNBOUND));
-		assertEquals(48, vm.getIP());
+	public void test0_3() {
+		testtmpl(0, 3, Unbound.UNBOUND);
 	}
 
 }
