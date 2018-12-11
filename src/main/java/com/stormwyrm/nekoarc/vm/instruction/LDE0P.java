bf8c01215a92ba3f75245c5ac1b16600201c3303
Rafael Sevilla <rsevilla@virtela.net>
2018-12-11 15:47:19 +0800

    LDE0P (Load From Environment 0 and Push) instruction

---
+++ src/main/java/com/stormwyrm/nekoarc/vm/instruction/LDE0P.java
@@ -0,0 +1,37 @@
+/*  Copyright (C) 2018 Rafael R. Sevilla
+
+    This file is part of NekoArc
+
+    NekoArc is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; either version 3 of the
+    License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+package com.stormwyrm.nekoarc.vm.instruction;
+
+import com.stormwyrm.nekoarc.NekoArcException;
+import com.stormwyrm.nekoarc.types.ArcThread;
+import com.stormwyrm.nekoarc.vm.Instruction;
+
+/**
+ * LDE0P - Load from Environment 0 and Push
+ */
+public class LDE0P implements Instruction {
+    /**
+     * LDE0P - Load from top-level environment and push
+     * @param thr The thread executing the instruction
+     * @throws NekoArcException on error
+     */
+    @Override
+    public void invoke(ArcThread thr) throws NekoArcException {
+        thr.push(thr.setAcc(thr.getenv(0, thr.smallInstArg() & 0xff)));
+    }
+}
