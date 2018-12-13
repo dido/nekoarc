a73178d3f4044fd0b4df7c502c1a9382d3bac18d
Dido Sevilla <dido@stormwyrm.com>
2018-12-13 10:15:58 +0800

    remove deprecated and use Op to generate code

---
+++ src/test/java/com/stormwyrm/nekoarc/MakeCode.java
@@ -0,0 +1,31 @@
+/*  Copyright (C) 2018 Rafael R. Sevilla
+
+    This file is part of NekoArc
+
+    NekoArc is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; either version 3 of the
+    License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+package com.stormwyrm.nekoarc;
+
+import com.stormwyrm.nekoarc.types.CodeGen;
+
+/**
+ * Code Maker interface for tests
+ */
+public interface MakeCode {
+    /**
+     * Make code. This generally used in a lambda expression that executes several emits against the codegen
+     * @param cg The code generator
+     */
+    void makeCode(CodeGen cg);
+}
