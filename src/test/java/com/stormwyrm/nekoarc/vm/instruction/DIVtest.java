e7c917472230ffeb2fcf6cf26e14876b4903361c
Dido Sevilla <dido@stormwyrm.com>
2018-12-13 11:40:06 +0800

    revise tests to use new conventions and templates

--- src/test/java/com/stormwyrm/nekoarc/vm/instruction/DIVtest.java
+++ src/test/java/com/stormwyrm/nekoarc/vm/instruction/DIVtest.java
@@ -4,93 +4,88 @@ import static org.junit.Assert.*;
 
 import com.stormwyrm.nekoarc.NekoArcException;
 import com.stormwyrm.nekoarc.Nil;
+import com.stormwyrm.nekoarc.Op;
+import com.stormwyrm.nekoarc.TestTemplate;
 import com.stormwyrm.nekoarc.types.ArcObject;
 import com.stormwyrm.nekoarc.types.Fixnum;
 import com.stormwyrm.nekoarc.types.Flonum;
 import com.stormwyrm.nekoarc.types.ArcThread;
 import org.junit.Test;
 
-public class DIVtest
-{
+public class DIVtest extends TestTemplate {
 	@Test
-	public void testFixnumDivFixnum1() throws NekoArcException
-	{
+	public void testFixnumDivFixnum1() {
 		// ldi 8; push; ldi 2; div; hlt
-		byte[] inst = {0x44, (byte) 0x08, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-				0x01,
-				0x44, (byte) 0x02, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-				0x18,
-				0x14};
-		ArcThread vm = new ArcThread(1024);
-		vm.load(inst);
-		vm.setAcc(Nil.NIL);
-		assertTrue(vm.runnable());
-		vm.run();
-		assertFalse(vm.runnable());
-		assertEquals(4, ((Fixnum)vm.getAcc()).fixnum);
-		assertEquals(13, vm.getIP());
+		doTestWithByteCode(new byte[]{0x44, (byte) 0x08, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+						0x01,
+						0x44, (byte) 0x02, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+						0x18,
+						0x14},
+				(cg)-> {
+					Op.LDI.emit(cg, 8);
+					Op.PUSH.emit(cg);
+					Op.LDI.emit(cg, 2);
+					Op.DIV.emit(cg);
+					Op.HLT.emit(cg);
+					return(cg);
+				}, Fixnum.get(4));
 	}
 
 	@Test
-	public void testFixnumDivFixnum2() throws NekoArcException
-	{
+	public void testFixnumDivFixnum2() throws NekoArcException {
 		// ldi 2; push; ldi 8; div; hlt
-		byte[] inst = {0x44, (byte) 0x02, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+		doTestWithByteCode(new byte[]{0x44, (byte) 0x02, (byte) 0x00, (byte) 0x00, (byte) 0x00,
 				0x01,
 				0x44, (byte) 0x08, (byte) 0x00, (byte) 0x00, (byte) 0x00,
 				0x18,
-				0x14};
-		ArcThread vm = new ArcThread(1024);
-		vm.load(inst);
-		vm.setAcc(Nil.NIL);
-		assertTrue(vm.runnable());
-		vm.run();
-		assertFalse(vm.runnable());
-		assertEquals(0, ((Fixnum)vm.getAcc()).fixnum);
-		assertEquals(13, vm.getIP());
+				0x14},
+				(cg) -> {
+					Op.LDI.emit(cg, 2);
+					Op.PUSH.emit(cg);
+					Op.LDI.emit(cg, 8);
+					Op.DIV.emit(cg);
+					Op.HLT.emit(cg);
+					return(cg);
+				}, Fixnum.ZERO);
 	}
 
 	@Test
-	public void testFlonumDivFixnum() throws NekoArcException
-	{
+	public void testFlonumDivFixnum() throws NekoArcException {
 		// ldl 0; push; ldi 2; div; hlt
-		byte[] inst = {0x43, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+		doTestWithByteCode(new byte[]{0x43, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
 				0x01,
 				0x44, (byte) 0x02, (byte) 0x00, (byte) 0x00, (byte) 0x00,
 				0x18,
-				0x14};
-		ArcObject[] literals = new ArcObject[1];
-		literals[0] = new Flonum(3.14);
-		ArcThread vm = new ArcThread(1024);
-		vm.load(inst, literals);
-		vm.setAcc(Nil.NIL);
-		assertTrue(vm.runnable());
-		vm.run();
-		assertFalse(vm.runnable());
-		assertEquals(1.57, ((Flonum)vm.getAcc()).flonum, 1e-6);
-		assertEquals(13, vm.getIP());
+				0x14},
+				(cg) -> {
+					Op.LDL.emit(cg, "pi");
+					Op.PUSH.emit(cg);
+					Op.LDI.emit(cg, 2);
+					Op.DIV.emit(cg);
+					Op.HLT.emit(cg);
+					cg.literal("pi", new Flonum(3.1415926535));
+					return(cg);
+			}, (t)-> assertEquals(1.570796325, ((Flonum)t.getAcc()).flonum, 1e-6));
 	}
 
 	@Test
-	public void testFlonumDivFlonum() throws NekoArcException
-	{
+	public void testFlonumDivFlonum() throws NekoArcException {
 		// ldl 0; push; ldl 1; div; hlt
-		byte[] inst = {0x43, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+		doTestWithByteCode(new byte[]{0x43, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
 				0x01,
 				0x43, (byte) 0x01, (byte) 0x00, (byte) 0x00, (byte) 0x00,
 				0x18,
-				0x14};
-		ArcObject[] literals = new ArcObject[2];
-		literals[0] = new Flonum(3.14);
-		literals[1] = new Flonum(2.71);
-		ArcThread vm = new ArcThread(1024);
-		vm.load(inst, literals);
-		vm.setAcc(Nil.NIL);
-		assertTrue(vm.runnable());
-		vm.run();
-		assertFalse(vm.runnable());
-		assertEquals(1.158671586715867, ((Flonum)vm.getAcc()).flonum, 1e-6);
-		assertEquals(13, vm.getIP());
+				0x14},
+				(cg) -> {
+					Op.LDL.emit(cg, "pi");
+					Op.PUSH.emit(cg);
+					Op.LDL.emit(cg, "e");
+					Op.DIV.emit(cg);
+					Op.HLT.emit(cg);
+					cg.literal("pi", new Flonum(3.1415926535));
+					cg.literal("e", new Flonum(2.7182818285));
+					return(cg);
+			}, (t)-> assertEquals(1.155727349740476, ((Flonum)t.getAcc()).flonum, 1e-6));
 	}
 
 }
