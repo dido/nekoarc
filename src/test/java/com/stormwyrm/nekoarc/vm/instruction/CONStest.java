a6791a808efe1a68c370cee3bb3f88aa35b2deec
Dido Sevilla <dido@stormwyrm.com>
2018-12-13 11:58:58 +0800

    revise test to remove deprecated and use templates

--- src/test/java/com/stormwyrm/nekoarc/vm/instruction/CONStest.java
+++ src/test/java/com/stormwyrm/nekoarc/vm/instruction/CONStest.java
@@ -3,31 +3,36 @@ package com.stormwyrm.nekoarc.vm.instruction;
 import static org.junit.Assert.*;
 
 import com.stormwyrm.nekoarc.Nil;
+import com.stormwyrm.nekoarc.Op;
+import com.stormwyrm.nekoarc.TestTemplate;
+import com.stormwyrm.nekoarc.types.Cons;
 import com.stormwyrm.nekoarc.types.Fixnum;
 import com.stormwyrm.nekoarc.types.ArcThread;
 import org.junit.Test;
 
-public class CONStest
-{
+public class CONStest extends TestTemplate {
 
 	@Test
-	public void test()
-	{
+	public void test() {
 		// ldi 2; push; ldi 1; cons; hlt
-        byte[] inst = {0x44, (byte) 0x02, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+        doTestWithByteCode(new byte[]{0x44, (byte) 0x02, (byte) 0x00, (byte) 0x00, (byte) 0x00,
                 0x01,
                 0x44, (byte) 0x01, (byte) 0x00, (byte) 0x00, (byte) 0x00,
                 0x19,
-                0x14};
-		ArcThread vm = new ArcThread(1024);
-		vm.load(inst);
-		vm.setAcc(Nil.NIL);
-		assertTrue(vm.runnable());
-		vm.run();
-		assertFalse(vm.runnable());
-		assertEquals(2, ((Fixnum) vm.getAcc().car()).fixnum);
-		assertEquals(1, ((Fixnum) vm.getAcc().cdr()).fixnum);
-		assertEquals(13, vm.getIP());
+                0x14},
+				(cg) ->{
+        	Op.LDI.emit(cg, 2);
+        	Op.PUSH.emit(cg);
+        	Op.LDI.emit(cg, 1);
+        	Op.CONS.emit(cg);
+        	Op.HLT.emit(cg);
+        	return(cg);
+				},
+				(t)->{
+        	assertTrue(t.getAcc() instanceof Cons);
+			assertEquals(2, ((Fixnum)t.getAcc().car()).fixnum);
+			assertEquals(1, ((Fixnum)t.getAcc().cdr()).fixnum);
+				});
 	}
 
 }
