4d26bd2c90c40bc03e6b89ba5e2d8227c3bff95f
Dido Sevilla <dido@stormwyrm.com>
2018-12-16 00:36:14 +0800

    tests for on-err/err/details

---
+++ src/test/java/com/stormwyrm/nekoarc/functions/OnErrTest.java
@@ -0,0 +1,45 @@
+package com.stormwyrm.nekoarc.functions;
+
+import com.stormwyrm.nekoarc.Op;
+import com.stormwyrm.nekoarc.TestTemplate;
+import com.stormwyrm.nekoarc.functions.typehandling.Sym;
+import com.stormwyrm.nekoarc.types.AString;
+import com.stormwyrm.nekoarc.types.Symbol;
+import org.junit.Test;
+
+public class OnErrTest extends TestTemplate {
+    @Test
+    public void testErrOnErr() {
+        doTest((cg) -> {
+            Op.ENV.emit(cg, 0, 0, 0);
+            Op.CLS.emit(cg, "handler");
+            Op.PUSH.emit(cg);
+            Op.CLS.emit(cg, "thunk");
+            Op.PUSH.emit(cg);
+            Op.LDG.emit(cg, "on-err");
+            Op.APPLY.emit(cg, 2);
+            Op.RET.emit(cg);
+            cg.label("handler", Op.ENV.emit(cg, 1, 0, 0));
+            Op.LDLP.emit(cg, "goterr");
+            Op.CONT.emit(cg, "cont1");
+            Op.LDE0P.emit(cg, 0);
+            Op.LDG.emit(cg, "details");
+            Op.APPLY.emit(cg, 1);
+            cg.label("cont1", Op.ADD.emit(cg));
+            Op.RET.emit(cg);
+            cg.label("thunk", Op.ENV.emit(cg, 0, 0, 0));
+            Op.LDLP.emit(cg, "wcatooex");
+            Op.LDG.emit(cg, "err");
+            Op.APPLY.emit(cg, 1);
+            Op.RET.emit(cg);
+            cg.literal("on-err", Symbol.intern("on-err"));
+            cg.literal("err", Symbol.intern("err"));
+            cg.literal("details", Symbol.intern("details"));
+            cg.literal("goterr", new AString("got error "));
+            cg.literal("wcatooex", new AString("we can also throw our own exceptions"));
+            return(cg);
+                },
+                new AString("got error we can also throw our own exceptions"));
+    }
+
+}
