c16b50f9e1af8e5713378ef1820ce6d928f25594
Rafael Sevilla <rsevilla@virtela.net>
2018-12-27 14:37:39 +0800

    javadoc

--- src/main/java/com/stormwyrm/nekoarc/types/Cons.java
+++ src/main/java/com/stormwyrm/nekoarc/types/Cons.java
@@ -1,3 +1,20 @@
+/*  Copyright (C) 2018 Rafael R. Sevilla
+
+    This file is part of NekoArc
+
+    NekoArc is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; either version 3 of the
+    License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
 package com.stormwyrm.nekoarc.types;
 
 import com.stormwyrm.nekoarc.InvokeThread;
@@ -6,60 +23,86 @@ import com.stormwyrm.nekoarc.Nil;
 
 import java.util.Iterator;
 
-public class Cons extends ArcObject implements Iterable<ArcObject>
-{
+/**
+ * Cons cells
+ */
+public class Cons extends ArcObject implements Iterable<ArcObject> {
 	public static final ArcObject TYPE = Symbol.intern("cons");
 	private ArcObject car;
 	private ArcObject cdr;
 
-	public Cons()
-	{
+	/**
+	 * Create an empty cons cell with car and cdr both nil
+	 */
+	public Cons() {
 	    this.car = this.cdr = Nil.NIL;
 	}
 
-	public Cons(ArcObject car, ArcObject cdr)
-	{
+	/**
+	 * Create a cons cell.
+	 * @param car The value of the car
+	 * @param cdr The value of the cdr
+	 */
+	public Cons(ArcObject car, ArcObject cdr) {
 		this.car = car;
 		this.cdr = cdr;
 	}
 
 	@Override
-	public ArcObject type()
-	{
+	public ArcObject type() {
 		return(TYPE);
 	}
 
+	/**
+	 * The car of this cons cell
+	 * @return Return the car of the cons ceell
+	 */
 	@Override
-	public ArcObject car()
-	{
+	public ArcObject car() {
 		return(car);
 	}
 
+	/**
+	 * The cdr of this cons cell
+	 * @return the cdr
+	 */
 	@Override
-	public ArcObject cdr()
-	{
+	public ArcObject cdr() {
 		return(cdr);
 	}
 
+	/**
+	 * Set the car of the cons cell
+	 * @param ncar The new value of the cell's car
+	 * @return the car
+	 */
 	@Override
-	public ArcObject scar(ArcObject ncar)
-	{
-		this.car = ncar;
-		return(ncar);
+	public ArcObject scar(ArcObject ncar) {
+		return(this.car = ncar);
 	}
 
+	/**
+	 * Set the cdr of the cons cell
+	 * @param ncdr the new value of the cell's cdr
+	 * @return the new value
+	 */
 	@Override
-	public ArcObject scdr(ArcObject ncdr)
-	{
-		this.cdr = ncdr;
-		return(ncdr);
+	public ArcObject scdr(ArcObject ncdr) {
+		return(this.cdr = ncdr);
 	}
-	
+
+	/**
+	 * Set a reference into the cons cell. It treas the cons cell as a list, and will
+	 * iterate down the cdr chain as many times as the index
+	 * @param value The new value to assign to the index
+	 * @param idx The index into the list
+	 * @return The value that was set.
+	 */
 	@Override
-	public ArcObject sref(ArcObject value, ArcObject idx)
-	{
+	public ArcObject sref(ArcObject value, ArcObject idx) {
 		Fixnum index = Fixnum.cast(idx, this);
-		return(nth(index.fixnum).scar(value));
+		nth(index.fixnum).scar(value);
+		return(value);
 	}
 
 	@Override
@@ -74,8 +117,12 @@ public class Cons extends ArcObject implements Iterable<ArcObject>
 	private static class OOB extends RuntimeException {
 	}
 
-	public Cons nth(long idx)
-	{
+	/**
+	 * Get the nth item in a list of cons cells
+	 * @param idx The index
+	 * @return The item at that index if it is within the list
+	 */
+	public Cons nth(long idx) {
 		try {
 			return((Cons)nth(this, idx));
 		} catch (OOB oob) {
@@ -83,8 +130,13 @@ public class Cons extends ArcObject implements Iterable<ArcObject>
 		}
 	}
 
-	private static ArcObject nth(ArcObject c, long idx)
-	{
+	/**
+	 * Get the nth item from an object, assuming it to have a valid cdr
+	 * @param c The object
+	 * @param idx the index
+	 * @return The value at that index, if available
+	 */
+	private static ArcObject nth(ArcObject c, long idx) {
 		while (idx > 0) {
 			if (c.cdr() instanceof Nil)
 				throw new OOB();
@@ -94,6 +146,10 @@ public class Cons extends ArcObject implements Iterable<ArcObject>
 		return(c);
 	}
 
+	/**
+	 * Get an iterator for the list of cons cells
+	 * @return The iterator
+	 */
     @Override
     public Iterator<ArcObject> iterator() {
         final ArcObject self = this;
@@ -120,6 +176,11 @@ public class Cons extends ArcObject implements Iterable<ArcObject>
         });
     }
 
+	/**
+	 * Is the other object equal to this one?
+	 * @param other the object to compare with
+	 * @return true if they are structurally equal
+	 */
 	@Override
 	public boolean iso(ArcObject other) {
 	    if (this.is(other))
@@ -132,6 +193,10 @@ public class Cons extends ArcObject implements Iterable<ArcObject>
 	}
 
 
+	/**
+	 * Get a string representation of the cons cell as a list.
+	 * @return The string representation of the conses
+	 */
     public String toString() {
         StringBuilder sb = new StringBuilder("(");
         // FIXME: recursion might go on forever if there are any cycles
@@ -151,19 +216,35 @@ public class Cons extends ArcObject implements Iterable<ArcObject>
         return(sb.toString());
     }
 
-    @Override
-	public int requiredArgs()
-	{
+	/**
+	 * Required args to apply a list
+	 * @return Always 1
+	 */
+	@Override
+	public int requiredArgs() {
 		return(1);
 	}
 
+	/**
+	 * Do a functional application of a list of conses. This will take only one argument, which
+	 * must be something that can be cast to a fixnum (n), and return the nth index into the list of
+	 * conses.
+	 * @param ithr The invocation thread.
+	 * @return The value at the nth-index of the list of conses
+	 */
 	@Override
-	public ArcObject invoke(InvokeThread ithr)
-	{
+	public ArcObject invoke(InvokeThread ithr) {
 		Fixnum idx = Fixnum.cast(ithr.getenv(0, 0), this);
 		return(this.nth(idx.fixnum).car());
 	}
 
+	/**
+	 * Coerce the cons to some other type. The only valid coercions are to strings and vectors. All
+	 * other attempts will result in an error
+	 * @param newtype The new type to convert to
+	 * @param extra additional parameters for the conversion, if any
+	 * @return The representation of the cons to the new type.
+	 */
 	@Override
 	public ArcObject coerce(ArcObject newtype, ArcObject extra) {
 		if (newtype == Symbol.intern("string")) {
