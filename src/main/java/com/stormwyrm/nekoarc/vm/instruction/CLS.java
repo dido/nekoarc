309f59b8041c9e838dc7b3e8a79872269c275a9c
Dido Sevilla <dido@stormwyrm.com>
2018-12-30 13:41:09 +0800

    new closure constructor

--- src/main/java/com/stormwyrm/nekoarc/vm/instruction/CLS.java
+++ src/main/java/com/stormwyrm/nekoarc/vm/instruction/CLS.java
@@ -1,3 +1,20 @@
+/*  Copyright (C) 2018 Rafael R. Sevilla
+
+    This file is part of NekoArc
+
+    NekoArc is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; either version 3 of the
+    License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
 package com.stormwyrm.nekoarc.vm.instruction;
 
 import com.stormwyrm.nekoarc.NekoArcException;
@@ -6,13 +23,19 @@ import com.stormwyrm.nekoarc.types.Fixnum;
 import com.stormwyrm.nekoarc.vm.Instruction;
 import com.stormwyrm.nekoarc.types.ArcThread;
 
-public class CLS implements Instruction
-{
+/**
+ * CLS - Create a closure
+ */
+public class CLS implements Instruction {
+	/**
+	 * Create a new closure.
+	 * @param thr The thread executing the instruction
+	 * @throws NekoArcException on error
+	 */
 	@Override
-	public void invoke(ArcThread thr) throws NekoArcException
-	{
+	public void invoke(ArcThread thr) throws NekoArcException {
 		int target = thr.instArg() + thr.getIP();
-		Closure clos = new Closure(thr.heapenv(), Fixnum.get(target));
+		Closure clos = new Closure(thr.heapenv(), target);
 		thr.setAcc(clos);
 	}
 
