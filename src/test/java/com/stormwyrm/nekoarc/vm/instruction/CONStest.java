6a84545fc6567fae8c8d2fc54b77030ca87b0a28
Dido Sevilla <dido@stormwyrm.com>
2019-01-01 11:29:49 +0800

    update code generation to use new conventions

--- src/test/java/com/stormwyrm/nekoarc/vm/instruction/CONStest.java
+++ src/test/java/com/stormwyrm/nekoarc/vm/instruction/CONStest.java
@@ -1,3 +1,20 @@
+/*  Copyright (C) 2018 Rafael R. Sevilla
+
+    This file is part of NekoArc
+
+    NekoArc is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation; either version 3 of the
+    License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
 package com.stormwyrm.nekoarc.vm.instruction;
 
 import static org.junit.Assert.*;
@@ -21,11 +38,13 @@ public class CONStest extends TestTemplate {
                 0x19,
                 0x14},
 				(cg) ->{
+        	cg.startCode();
         	Op.LDI.emit(cg, 2);
         	Op.PUSH.emit(cg);
         	Op.LDI.emit(cg, 1);
         	Op.CONS.emit(cg);
         	Op.HLT.emit(cg);
+        	cg.endCode();
         	return(cg);
 				},
 				(t)->{
