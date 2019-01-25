cf907097767a06d288979c99a833b754fcccba9f
Rafael Sevilla <rsevilla@virtela.net>
2019-01-25 15:26:55 +0800

    test for rune marshalling

--- src/test/java/com/stormwyrm/nekoarc/types/RuneTest.java
+++ src/test/java/com/stormwyrm/nekoarc/types/RuneTest.java
@@ -21,7 +21,10 @@
 package com.stormwyrm.nekoarc.types;
 
 import com.stormwyrm.nekoarc.Nil;
+import com.stormwyrm.nekoarc.ciel.Ciel;
 import org.junit.Test;
+
+import java.util.Random;
 
 import static org.junit.Assert.*;
 
@@ -42,5 +45,24 @@ public class RuneTest {
         result = r.coerce(Symbol.intern("string"), Nil.NIL);
         assertEquals("string", result.type().toString());
         assertTrue(result.is(new AString("龍")));
+    }
+
+    @Test
+    public void testMarshal() {
+        OutString os = new OutString();
+        // Generate 100 random runes, and marshal them into os.
+        Random rng = new Random();
+        ArcObject[] nums = new ArcObject[100];
+        for (int i=0; i<100; i++) {
+            nums[i] = Rune.get(rng.nextInt(0x10fff));
+            nums[i].marshal(os);
+        }
+        InString is = new InString(os.insideBytes(), "");
+        // Now, load the marshalled data.
+        Ciel c = new Ciel(is);
+        c.load();
+        // The stack of the Ciel object should contain each of the fixnums we have in nums in reverse order
+        for (int i=99; i>=0; i--)
+            assertTrue(nums[i].iso(c.pop()));
     }
 }
