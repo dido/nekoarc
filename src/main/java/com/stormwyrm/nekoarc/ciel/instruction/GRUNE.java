6c45a89957f4e2db68e1097e806e74600f42a645
Rafael Sevilla <rsevilla@virtela.net>
2019-01-25 15:24:41 +0800

    grune instruction

---
+++ src/main/java/com/stormwyrm/nekoarc/ciel/instruction/GRUNE.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2019 Rafael R. Sevilla
+ *
+ * This file is part of NekoArc
+ *
+ * NekoArc is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.stormwyrm.nekoarc.ciel.instruction;
+
+import com.stormwyrm.nekoarc.NekoArcException;
+import com.stormwyrm.nekoarc.ciel.Ciel;
+import com.stormwyrm.nekoarc.ciel.CielInstruction;
+import com.stormwyrm.nekoarc.types.Rune;
+
+public class GRUNE implements CielInstruction {
+    @Override
+    public void invoke(Ciel c) {
+        long rune = c.readLong();
+        if (rune < 0 || rune > 0x10ffff)
+            throw new NekoArcException("invalid Unicode rune in marshalled data");
+        c.push(Rune.get((int) rune));
+    }
+}
